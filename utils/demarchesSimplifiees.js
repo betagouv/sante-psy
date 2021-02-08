2ce36c7f1fe58e18b2096e3388d21504610a5a63
Paul Leclercq <paleclercq@gmail.com>
2021-02-08 12:13:10 +0100

    temporary: faking psychologist list

--- utils/demarchesSimplifiees.js
+++ utils/demarchesSimplifiees.js
@@ -1,7 +1,55 @@
 const graphql = require('../utils/graphql');
 
 async function getPsychologistList(input) {
-  return graphql.requestPsychologist();
+  const psyList = [
+    {
+      'name': 'name1',
+      'address': '22 rue du four, 54000 Metz',
+      'phone': '010000000',
+    },
+    {
+      'name': 'name2',
+      'address': '22 rue du four, 54000 Metz',
+      'phone': '010000000',
+    },
+    {
+      'name': 'name3',
+      'address': '22 rue du four, 54000 Metz',
+      'phone': '010000000',
+    },
+    {
+      'name': 'name4',
+      'address': '22 rue du four, 54000 Metz',
+      'phone': '010000000',
+    },
+    {
+      'name': 'name5',
+      'address': '22 rue du four, 54000 Metz',
+      'phone': '010000000',
+    },
+    {
+      'name': 'name6',
+      'address': '22 rue du four, 54000 Metz',
+      'phone': '010000000',
+    },
+    {
+      'name': 'name7',
+      'address': '22 rue du four, 54000 Metz',
+      'phone': '010000000',
+    },
+    {
+      'name': 'name8',
+      'address': '22 rue du four, 54000 Metz',
+      'phone': '010000000',
+    },
+    {
+      'name': 'name9',
+      'address': '22 rue du four, 54000 Metz',
+      'phone': '010000000',
+    },
+  ];
+  return psyList;
+  // return graphql.requestPsychologist();
 }
   
 exports.getPsychologistList = getPsychologistList;
