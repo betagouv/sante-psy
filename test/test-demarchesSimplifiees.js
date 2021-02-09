8441cd48b204b463dc51eec901b84ef830914ab3
Paul Leclercq <paleclercq@gmail.com>
2021-02-09 09:58:20 +0100

    test: parse psychologist from API

--- test/test-demarchesSimplifiees.js
+++ test/test-demarchesSimplifiees.js
@@ -2,51 +2,22 @@ require('dotenv').config();
 const rewire = require('rewire');
 const should = require('chai').should();
 const nock = require('nock');
-const testUsers = require('./psychologist.json');
+const testDossiers = require('./dossier.json');
 
 const demarchesSimplifiees = rewire('../utils/demarchesSimplifiees.js');
 
 describe('Demarches Simplifiess', () => {
-  describe('getPsychologistList', () => {
-    //@TODO testUsers should match API's schema
-    it('should return an array of psychologists', async () => {
-      const apiCall = nock(/.*demarches-simplifiees.fr/)
-            .post(/^.*api\/v2\/graphql/)
-            .reply(200, testUsers);
-            
-      const output = await demarchesSimplifiees.getPsychologistList();
-      apiCall.isDone().should.be.true;
-    });
-  });
   describe('parsePsychologist', () => {
-    //@TODO testUsers should match API's schema
     it('should return an array of psychologists from a JSON', async () => {
-      const apiResponse = { demarche: { id: 'UHJvY2VkdXJlLTM5NzQ3', dossiers: {
-        nodes:  [ { champs:
-          [ { id: 'Q2hhbXAtMTYwMTE4Ng==',
-            label: 'Nouveau champ Texte',
-            stringValue: 'Nouveau champ Texte 2ème Dossier' },
-          { id: 'Q2hhbXAtMTYwMzgwNQ==',
-            label: 'Nouveau champ Texte',
-            stringValue: 'Autre champ Texte 2ème Dossier' },
-          { id: 'Q2hhbXAtMTYxODcwMw==',
-            label: 'Nouveau champ Texte',
-            stringValue: '' },
-          { id: 'Q2hhbXAtMTYxODcwNg==',
-            label: 'Nouveau champ Texte',
-            stringValue: '' } ]
-        } ]
-      } 
-      } 
-      }
+      const apiResponse = testDossiers
 
       const parsePsychologist = demarchesSimplifiees.__get__('parsePsychologist');
       const output = parsePsychologist(apiResponse)
 
       const result = [{ 
-        name:'in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ',
-        address:'in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ',
-        phone:'in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
+        name:'Nouveau champ Texte 1er Dossier',
+        address:'Nouveau champ Texte 1er Dossier',
+        phone:'Nouveau champ Texte 1er Dossier'
       },
       { name: 'Nouveau champ Texte 2ème Dossier',
         address: 'Nouveau champ Texte 2ème Dossier',
@@ -54,7 +25,7 @@ describe('Demarches Simplifiess', () => {
       }
       ]
 
-      output.should.equal(result);
+      output.should.eql(result);
     });
   });
 });
--- utils/demarchesSimplifiees.js
+++ utils/demarchesSimplifiees.js
@@ -9,27 +9,26 @@ async function getPsychologistList() {
 exports.getPsychologistList = getPsychologistList;
 
 function parsePsychologist(apiResponse) {
-  console.debug("apiResponse", apiResponse)
+  console.debug("apiResponse", apiResponse.demarche.dossiers.nodes)
   const dossiers = apiResponse.demarche.dossiers.nodes
 
   if(dossiers.length > 0) {
     const psychologists = dossiers.map(dossier => {
-      console.debug('parsing dossier', dossier);
 
       //@TODO improve me, how to get a specific attribute without using 'find'
       const name = dossier.champs.find(champ => champ.label === 'Nouveau champ Texte').stringValue;
       const address = dossier.champs.find(champ => champ.label === 'Nouveau champ Texte').stringValue;
       const phone = dossier.champs.find(champ => champ.label === 'Nouveau champ Texte').stringValue;
       
-      
       const psy = { name, address, phone };
-      console.log('Parsed psy', psy);
       
-      psy; //@TODO how to return a value for a map ? it returns undefined
+      return psy;
     });
+    console.log("final parsed psychologists list", psychologists);
     return psychologists;
   } else {
-    throw 'Aucun psychologue enregistré pour le moment'
+    console.error('Aucun psychologist trouvé.');
+    
+    return [];
   }
-
 }
