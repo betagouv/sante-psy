03fe4b8eb7b94b4714a6cd6b27bf66c7ea129a5c
Estelle Comment <estelle.comment@gmail.com>
2021-02-10 13:56:19 +0100

    Rename table

--- migrations/20210210114608_init.js
+++ migrations/20210210114608_init.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex) {
   return knex.schema
-  .createTable('appointment', (table) => {
+  .createTable('appointments', (table) => {
     table.datetime('date').notNullable().defaultTo(knex.fn.now());
   })
 };
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTable('appointment')
+    .dropTable('appointments')
 };
