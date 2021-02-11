3eca05065a93717ba166b5544ec813c281c056d8
Estelle Comment <estelle.comment@gmail.com>
2021-02-11 16:22:51 +0100

    Use config.js to get env var

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,8 @@
-console.log('DATABASE_URL', process.env.DATABASE_URL)
+const config = require('./utils/config')
 
 module.exports = {
   client: "pg",
-  connection: process.env.DATABASE_URL,
+  connection: config.databaseUrl,
   acquireConnectionTimeout: 10000,
   migrations: {
     tableName: "knex_migrations"
