4593c0895e0017be8889883e7e40cab90cabb75b
Estelle Comment <estelle.comment@gmail.com>
2021-02-12 11:20:02 +0100

    Display list of appointments

--- controllers/myAppointmentsController.js
+++ controllers/myAppointmentsController.js
@@ -1,4 +1,7 @@
+const db = require('../utils/db')
 
-module.exports.myAppointments = (req, res) => {
-  res.render('myAppointments')
+module.exports.myAppointments = async (req, res) => {
+  const appointments = await db.getAppointments()
+
+  res.render('myAppointments', { appointments: appointments })
 }
