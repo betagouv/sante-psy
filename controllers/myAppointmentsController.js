297cdd984e1978101d50d913f7d8d5aaeeadde81
Estelle Comment <estelle.comment@gmail.com>
2021-02-12 16:14:17 +0100

    Display success and error messages.

--- controllers/myAppointmentsController.js
+++ controllers/myAppointmentsController.js
@@ -21,9 +21,11 @@ module.exports.createNewAppointment = async (req, res) => {
 
   try {
     await db.insertAppointment(date)
+    req.flash('info', `Le rendez-vous du ${dateString} a bien été créé.`)
     return res.redirect('/mes-rendez-vous')
   } catch (err) {
     req.flash('error', 'Erreur. Le rendez-vous n\'est pas créé. Vous pouvez réessayer.')
     console.error('Erreur pour créer le rendez-vous', err)
+    return res.redirect('/nouveau-rendez-vous')
   }
 }
