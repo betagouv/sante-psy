f694554b58d20184fd962ed41b10f79bc1c7a6da
Estelle Comment <estelle.comment@gmail.com>
2021-02-12 17:34:18 +0100

    npm install body-parser

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 const flash = require('connect-flash');
