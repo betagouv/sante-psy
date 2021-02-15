8981cf4ac8c6e5e5f02bc66dfa8e9826416355c9
Estelle Comment <estelle.comment@gmail.com>
2021-02-15 11:42:55 +0100

    First unit tests

---
+++ test/test-appointmentsController.js
@@ -0,0 +1,72 @@
+/* eslint-disable func-names */
+const app = require('../index')
+const chai = require('chai')
+const db = require('../utils/db')
+const sinon = require('sinon')
+
+describe('appointmentsController', function() {
+  let insertAppointmentStub
+
+  beforeEach(function(done) {
+    insertAppointmentStub = sinon.stub(db, 'insertAppointment')
+      .returns(Promise.resolve())
+    done()
+  })
+
+  afterEach(function(done) {
+    insertAppointmentStub.restore()
+    done()
+  })
+
+
+  it('should create appointment', function(done) {
+    chai.request(app)
+      .post('/creer-nouveau-rendez-vous')
+      .redirects(0) // block redirects, we don't want to test them
+      .type('form')
+      .send({
+        'patient-name': 'Georges Perec',
+        date: '09/02/2021',
+        'iso-date': '2021-02-09',
+      })
+      .end((err, res) => {
+        res.should.redirectTo('/mes-rendez-vous')
+        sinon.assert.called(insertAppointmentStub)
+        done()
+      })
+  })
+
+  it('should refuse invalid date', function(done) {
+    chai.request(app)
+      .post('/creer-nouveau-rendez-vous')
+      .redirects(0) // block redirects, we don't want to test them
+      .type('form')
+      .send({
+        'patient-name': 'Georges Perec',
+        date: '09/02/2021',
+        'iso-date': '2021-02-09kk',
+      })
+      .end((err, res) => {
+        res.should.redirectTo('/nouveau-rendez-vous')
+        sinon.assert.notCalled(insertAppointmentStub)
+        done()
+      })
+  })
+
+  it('should ignore the date input and use the iso-date', function(done) {
+    chai.request(app)
+      .post('/creer-nouveau-rendez-vous')
+      .redirects(0) // block redirects, we don't want to test them
+      .type('form')
+      .send({
+        'patient-name': 'Georges Perec',
+        date: '12/02/2021 kfjhksdhf',
+        'iso-date': '2021-02-09',
+      })
+      .end((err, res) => {
+        res.should.redirectTo('/mes-rendez-vous')
+        sinon.assert.called(insertAppointmentStub)
+        done()
+      })
+  })
+})
