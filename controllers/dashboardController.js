6a675cf33e786dd9a912e55f3fc0e5bb67056b3d
eml-trm <termeau.e@gmail.com>
2021-02-18 14:14:46 +0100

    Rename dashboard method and group promises

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,20 +1,21 @@
+const { result } = require('lodash')
 const db = require('../utils/db')
 
-module.exports.getAppointmentsAndPatients = async function getAppointmentsAndPatients(req, res) {
+module.exports.dashboard = async function dashboard(req, res) {
   try {
-    const patients = await db.getPatients()
-    const appointments = await db.getAppointments()
+    const results = await Promise.all([db.getPatients(), db.getAppointments()])
+    const patients = results[0]
+    const appointments = results[1]
 
     res.render('dashboard', {
       appointments: appointments,
       patients: patients
     });
   } catch (err) {
-    console.error('getAppointmentsAndPatients', err);
+    console.error('dashboard', err);
     res.render('dashboard', {
       appointments: [],
-      patients: [],
-      errors: [err],
+      patients: []
     });
   }
 }
