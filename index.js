b90a8f417046860689f25730790460c8e320b7b8
Paul Leclercq <paleclercq@gmail.com>
2021-02-23 15:15:43 +0100

    Feat/rate limit api (#34)
    * feat(api): add pagination
    
    * fix(debug): use a env variable to desactivate console.debug
    
    * review(api-cursor): use undefined instead of empty string when there is no more page to load
    
    * review: french comments
    
    * review(getPsychologist): use timer inside utils function to clean up the controller
    
    * lint: Unexpected unnamed function
    
    * wip: get the latest cursor
    
    * review: change functions name | debug feature flag name
    
    * wip: saving to PG from DS
    
    * wip
    
    * wip: needs to create PG tables
    
    * wip
    
    * wip(test): PG / DS api
    
    * wip(cursor): save cursor and use it to query DS API
    
    * feat: add dossierNumber as primary key
    
    * feat(dossierNumber): add demarche Id before dossier number
    
    * clean: remove old todo
    
    * feat(uuid): use uuid with dossier and demarche id to store psychologist
    
    * sql(psychologists): use uuid
    
    * fix: use last name and first names instead of name
    
    * fix(test): all tests work with a PG container
    
    * lint
    
    * wip: trying dockercompose on github workflow
    
    * package: use mocha --exit to exit tests after run
    
    * docker-compose: use env.sample
    
    * ci: use docker-compose in detached mode
    
    * ci: copy .env.sample to .env
    
    * fix(test): using env.sample ci config
    
    * ci(test): catch error when cleaning data of PG container
    
    * ci(test): catch error when cleaning data of PG container
    
    * ci(test): remove if psychologist data exist in beforeEach
    
    * ci(test): run test inside container
    
    * ci(docker): use --abort-on-container-exit to run test
    
    * ci(docker): use npm install
    
    * ci(docker): change node_modules folder
    
    * ci(docker): use npm install first before starting docker-compose
    
    * doc(docker): test on local machine
    
    * test(importData): test upsert
    
    * feat: add rate limit to access website
    
    * lint
    
    error merge
    
    * review: remove 404 redirection

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const express = require('express');
 const path = require('path');
 const flash = require('connect-flash');
 const session = require('express-session');
+const rateLimit = require("express-rate-limit");
+const slowDown = require("express-slow-down");
 
 const config = require('./utils/config');
 const format = require('./utils/format');
@@ -60,10 +62,28 @@ app.use(function populate(req, res, next){
 })
 app.locals.format = format
 
+// prevent abuse
+const rateLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minute window
+  max: 1000, // start blocking after X requests for windowMs time
+  message: "Trop de requête venant de cette IP, veuillez essayer plus tard."
+});
+app.use(rateLimiter);
+
 app.get('/', landingController.getLanding);
 
 if (config.featurePsyList) {
-  app.get('/consulter-les-psychologues', psyListingController.getPsychologist);
+  // prevent abuse for some rules
+  const speedLimiter = slowDown({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    delayAfter: 100, // allow X requests per 5 minutes, then...
+    delayMs: 500 // begin adding 500ms of delay per request above 100:
+    // request # 101 is delayed by  500ms
+    // request # 102 is delayed by 1000ms
+    // request # 103 is delayed by 1500ms
+    // etc.
+  });
+  app.get('/consulter-les-psychologues', speedLimiter, psyListingController.getPsychologist);
 }
 
 if (config.featurePsyPages) {
