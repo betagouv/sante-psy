78202f46a383c9367b8d02ebb72a431fdeaa15e4
Estelle Comment <estelle.comment@gmail.com>
2021-03-01 14:05:42 +0100

    Test psyId is set

--- test/test-patientsController.js
+++ test/test-patientsController.js
@@ -38,6 +38,7 @@ describe('patientsController', function() {
 
           const patientsArray = await dbPatients.getPatients(psy.dossierNumber)
           expect(patientsArray).to.have.length(1)
+          expect(patientsArray[0].psychologistId).to.equal(psy.dossierNumber)
 
           return Promise.resolve()
         })
