0371daeae0ca8e304ad8e55fdf5dd11049aa1632
Estelle Comment <estelle.comment@gmail.com>
2021-03-01 12:31:45 +0100

    Add test for dashboardController

---
+++ test/test-dashboardController.js
@@ -0,0 +1,77 @@
+// todo(estellecomment) test that only my own patients and appointments are displayed.
+/* eslint-disable func-names */
+const app = require('../index')
+const chai = require('chai')
+const clean = require('./helper/clean')
+const cookie = require('../utils/cookie')
+const dbAppointments = require('../db/appointments')
+const dbPatients = require('../db/patients')
+const format = require('../utils/format')
+
+describe('dashboardController', function() {
+  describe('display dashaboard', function() {
+    beforeEach(async function(done) {
+      done()
+    })
+
+    afterEach(async function() {
+      await clean.cleanAllPatients()
+      await clean.cleanAllAppointments()
+      return Promise.resolve()
+    })
+
+    it('should display only my patients in dashboard', async function() {
+      const psy = {
+        dossierNumber: '9a42d12f-8328-4545-8da3-11250f876146',
+        email: 'valid@valid.org',
+      }
+      const anotherPsyId = '60014566-d8bf-4f01-94bf-27b31ca9275d'
+      const myPatient = await dbPatients.insertPatient('Ada', 'Lovelace', '12345678901', psy.dossierNumber)
+      const patientForAnotherPsy = await dbPatients.insertPatient('Stevie', 'Wonder', '34567890123', anotherPsyId)
+
+      return chai.request(app)
+        .get('/mes-seances')
+        .set('Cookie', `token=${cookie.getJwtTokenForUser(psy.email, psy)}`)
+        .redirects(0) // block redirects, we don't want to test them
+        .then(async (res) => {
+          // My patients are present
+          chai.assert.include(res.text, myPatient.firstNames)
+          chai.assert.include(res.text, myPatient.lastName)
+
+          // Other psy's patients are not listed
+          chai.assert.notInclude(res.text, patientForAnotherPsy.firstNames)
+          chai.assert.notInclude(res.text, patientForAnotherPsy.lastName)
+          return Promise.resolve()
+        })
+    })
+
+    it('should display only my appointments in dashboard', async function() {
+      const psy = {
+        dossierNumber: '9a42d12f-8328-4545-8da3-11250f876146',
+        email: 'valid@valid.org',
+      }
+      const anotherPsyId = '60014566-d8bf-4f01-94bf-27b31ca9275d'
+      const myPatient = await dbPatients.insertPatient('Ada', 'Lovelace', '12345678901', psy.dossierNumber)
+      const patientForAnotherPsy = await dbPatients.insertPatient('Stevie', 'Wonder', '34567890123', anotherPsyId)
+      const myAppointment =
+        await dbAppointments.insertAppointment(new Date('2021-03-01'), myPatient.id, psy.dossierNumber)
+      const appointmentForAnotherPsy =
+        await dbAppointments.insertAppointment(new Date('2021-12-12'), patientForAnotherPsy.id, anotherPsyId)
+
+      return chai.request(app)
+        .get('/mes-seances')
+        .set('Cookie', `token=${cookie.getJwtTokenForUser(psy.email, psy)}`)
+        .redirects(0) // block redirects, we don't want to test them
+        .then(async (res) => {
+          // My appointments are present
+          chai.assert.include(res.text, format.formatFrenchDate(myAppointment.appointmentDate))
+
+          // Other psy's patients are not listed
+          chai.assert.notInclude(res.text, format.formatFrenchDate(appointmentForAnotherPsy.appointmentDate))
+
+          return Promise.resolve()
+        })
+    })
+
+  })
+})
