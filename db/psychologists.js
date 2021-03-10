973fa8c827d05df5a4cf6c24a68e5fa76ff79c62
Estelle Comment <estelle.comment@gmail.com>
2021-03-10 18:46:23 +0100

    Add languages to psylist

--- db/psychologists.js
+++ db/psychologists.js
@@ -18,6 +18,7 @@ module.exports.getPsychologists = async () => {
       'phone',
       'website',
       'teleconsultation',
+      'languages',
       'description')
         .select()
         .from(module.exports.psychologistsTable)
