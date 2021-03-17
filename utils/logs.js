f80e2d14bf8668a264f98b99385a9440e743b519
Paul Leclercq <paleclercq@gmail.com>
2021-03-17 15:15:07 +0100

    fix(logs): use toLowerCase if user uses capitalized letters by mistake

--- utils/logs.js
+++ utils/logs.js
@@ -3,6 +3,6 @@ const crypto = require('crypto')
 
 module.exports.hashForLogs = function hashForLogs(logs = "") {
   return 'hash ' + crypto.createHmac('sha256', config.secretLogs)
-    .update(logs)
+    .update(logs.toLowerCase())
     .digest('hex');
 }
