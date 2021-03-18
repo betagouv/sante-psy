ad345ab9b31aea6077209dd2ec7244f0638ec037
Paul Leclercq <paleclercq@gmail.com>
2021-03-18 18:33:43 +0100

    feat(myPatients): add patient folder completed status

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -2,6 +2,11 @@ const cookie = require('../utils/cookie')
 const dbAppointments = require('../db/appointments')
 const dbPatient = require('../db/patients')
 
+function hasFolderCompleted(patient) {
+  return (patient.INE !== '') && patient.hasPrescription && patient.isStudentStatusVerified &&
+  patient.institutionName && (patient.doctorName !== null) && (patient.institutionName !== null)
+}
+
 module.exports.dashboard = async function dashboard(req, res) {
   try {
     const psychologistId = cookie.getCurrentPsyId(req)
@@ -12,9 +17,14 @@ module.exports.dashboard = async function dashboard(req, res) {
     const patients = results[0]
     const appointments = results[1]
 
+    const patientsWithFolderStatus = patients.map ( patient => {
+      patient.hasFolderCompleted = hasFolderCompleted(patient)
+      return patient;
+    });
+
     res.render('dashboard', {
       appointments: appointments,
-      patients: patients
+      patients: patientsWithFolderStatus
     });
   } catch (err) {
     req.flash('error', 'Impossible de charger les séances et les patients. Réessayez ultérieurement.')
