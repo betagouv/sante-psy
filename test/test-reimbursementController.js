ad4c896cf90a639296a6847ef8a40cb13cc34aa0
Estelle Comment <estelle.comment@gmail.com>
2021-03-25 14:01:38 +0100

    Test for updateConventionInfo

---
+++ test/test-reimbursementController.js
@@ -0,0 +1,52 @@
+const app = require('../index')
+const chai = require('chai')
+const clean = require('./helper/clean')
+const cookie = require('../utils/cookie')
+const dbPsychologists = require('../db/psychologists')
+const dbUniversities = require('../db/universities')
+
+describe('reimbursementController', () => {
+  describe('updateConventionInfo', () => {
+    let university
+    before(async () => {
+      university = await dbUniversities.insertUniversity('Cool U dude')
+    });
+
+    after(async () => {
+      await clean.cleanAllUniversities()
+    });
+
+    beforeEach((done) => {
+      done()
+    })
+
+    afterEach(async () => {
+      await clean.cleanAllPsychologists()
+      return
+    })
+
+    it('should updateConventionInfo', async () => {
+      const psyEmail = 'login@beta.gouv.fr'
+      await dbPsychologists.savePsychologistInPG([clean.getOnePsy(psyEmail, 'accepte', false)])
+      const psy = await dbPsychologists.getAcceptedPsychologistByEmail(psyEmail)
+      // Check that the fields we are testing are unset before test
+      chai.expect(psy.isConventionSigned).not.to.exist
+      chai.expect(psy.payingUniversityId).not.to.exist
+
+      return chai.request(app)
+        .post('/psychologue/api/renseigner-convention')
+        .set('Cookie', `token=${cookie.getJwtTokenForUser(psy.email, psy.dossierNumber)}`)
+        .redirects(0) // block redirects, we don't want to test them
+        .type('form')
+        .send({
+          signed: 'yes',
+          university: university.id,
+        })
+        .then(async () => {
+          const updatedPsy = await dbPsychologists.getAcceptedPsychologistByEmail(psyEmail)
+          chai.expect(updatedPsy.isConventionSigned).to.equal(true)
+          chai.expect(updatedPsy.payingUniversityId).to.equal(university.id)
+        })
+    })
+  })
+})
