94d76ff0f7778611386c5676c35e0687b2c67fe3
Paul Leclercq <paleclercq@gmail.com>
2021-04-08 17:09:29 +0200

    review(refacto): cleaner hasFolderCompleted tests and optional text for INE in view

--- test/test-dashboardController.js
+++ test/test-dashboardController.js
@@ -11,9 +11,9 @@ const format = require('../utils/format')
 
 describe('dashboardController', function() {
   describe('hasFolderCompleted', function() {
-    const hasFolderCompleted = dashboardController.__get__('hasFolderCompleted');
-    it('should return true if patient has all needed info', function() {
-      const patient =  {
+    let patient
+    beforeEach( function() {
+      patient =  {
         firstNames : "firstNames",
         lastName : "lastName",
         INE : "INE",
@@ -22,48 +22,58 @@ describe('dashboardController', function() {
         hasPrescription : true,
         psychologistId : "psychologistId",
         doctorName : "doctorName",
-        doctorAddress : "30000 Nîmes"
+        doctorAddress : "30000 Nîmes",
       }
+    })
 
+    const hasFolderCompleted = dashboardController.__get__('hasFolderCompleted');
+    it('should return true if patient has all needed info', function() {
       hasFolderCompleted(patient).should.equal(true);
     });
 
-    it('should return false if patient does not have all needed info (prescription, student status, doctor name)',
-      function() {
-        let patient =  {
-          firstNames : "firstNames",
-          lastName : "lastName",
-          INE : "INE",
-          institutionName : "institutionName",
-          isStudentStatusVerified : true,
-          hasPrescription : false,
-          psychologistId : "psychologistId",
-          doctorName : "doctorName",
-          doctorAddress : "30000 Nîmes",
-        }
-        hasFolderCompleted(patient).should.equal(false);
-        patient.doctorName=null;
-        patient.hasPrescription=true;
-        hasFolderCompleted(patient).should.equal(false);
-        patient.doctorName='doctorName';
-        patient.isStudentStatusVerified=false;
-        hasFolderCompleted(patient).should.equal(false);
-        patient.isStudentStatusVerified=true;
-        patient.INE='INE';
-        patient.doctorName="    ", // trim should work
-        hasFolderCompleted(patient).should.equal(false);
-        patient.doctorName="doctorName", // trim should work
-        patient.institutionName='    ';
-        hasFolderCompleted(patient).should.equal(false);
-        patient.doctorName="", 
-        patient.institutionName='institutionName';
-        hasFolderCompleted(patient).should.equal(false);
-        patient.doctorName="doctorName",
-        patient.doctorAddress='';
-        hasFolderCompleted(patient).should.equal(false);
-        patient.doctorAddress='    ';
-        hasFolderCompleted(patient).should.equal(false);
-      });
+    it('should return true if INE is missing', function() {
+      patient.INE=null;
+      hasFolderCompleted(patient).should.equal(true);
+    });
+
+    it('should return false if doctor name is missing', function() {
+      patient.doctorName=null;
+      hasFolderCompleted(patient).should.equal(false);
+    });
+
+
+    it('should return false if isStudentStatusVerified is false', function() {
+      patient.isStudentStatusVerified=false;
+      hasFolderCompleted(patient).should.equal(false);
+    });
+
+    it('should return false if hasPrescription is false', function() {
+      patient.hasPrescription=false;
+      hasFolderCompleted(patient).should.equal(false);
+    });
+
+    it('should return false if doctorName is a only white spaces', function() {
+      patient.doctorName="    ", // trim should work
+      hasFolderCompleted(patient).should.equal(false);
+    });
+    it('should return false if doctorName is an empty string', function() {
+      patient.doctorName="";
+      hasFolderCompleted(patient).should.equal(false);
+    });
+
+    it('should return false if institutionName is a only white spaces', function() {
+      patient.institutionName='    ';
+      hasFolderCompleted(patient).should.equal(false);
+    });
+
+    it('should return false if doctorAddress is an empty string', function() {
+      patient.doctorAddress='';
+      hasFolderCompleted(patient).should.equal(false);
+    });
+    it('should return false if doctor adress is a only white spaces', function() {
+      patient.doctorAddress='    ';
+      hasFolderCompleted(patient).should.equal(false);
+    });
   });
   describe('display dashaboard', function() {
     beforeEach(async function(done) {
