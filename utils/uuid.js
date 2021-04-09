607a027356d2e17af48fe39e9c5536065c9ec674
eml-trm <termeau.e@gmail.com>
2021-04-09 12:28:38 +0200

    refacto methode to generate uuid

--- utils/uuid.js
+++ utils/uuid.js
@@ -1,6 +1,11 @@
 const { v5: uuidv5 } = require('uuid');
 const config = require('./config');
 
-module.exports.generateUuidFromString = (id) => {
+const generateUuidFromString = (id) => {
   return uuidv5(id, config.uuidNamespace);
+}
+module.exports.generateUuidFromString = generateUuidFromString
+
+module.exports.randomUuid = function randomUuid(random = Math.random().toString()) {
+  return generateUuidFromString(random);
 }
