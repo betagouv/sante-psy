18040e4759fe657a3735d45f6505fb05e3a2eae3
Estelle Comment <estelle.comment@gmail.com>
2021-04-15 17:04:27 +0200

    Add extra field in migration

--- migrations/20210412162202_add-assignedUniversity.js
+++ migrations/20210412162202_add-assignedUniversity.js
@@ -3,11 +3,13 @@ const { psychologistsTable } = require('../db/psychologists')
 exports.up = function (knex) {
   return knex.schema.table(psychologistsTable, function (table) {
     table.uuid('assignedUniversityId');
+    table.renameColumn('payingUniversityId', 'declaredUniversityId')
   })
 }
 
 exports.down = function (knex) {
   return knex.schema.table(psychologistsTable, function (table) {
     table.dropColumn('assignedUniversityId');
+    table.renameColumn('declaredUniversityId', 'payingUniversityId')
   })
 };
