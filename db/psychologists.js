076ad95c8688a7bba15ac7caed27970aa6bcd1f9
Paul Leclercq <paleclercq@gmail.com>
2021-04-22 10:55:19 +0200

    fix(login): compare email in lowercase (#154)
    * fix(login): compare email in lowercase
    
    * review: add lower to personalEmail to compare email

--- db/psychologists.js
+++ db/psychologists.js
@@ -114,7 +114,9 @@ module.exports.getNumberOfPsychologists = async function getNumberOfPsychologist
 module.exports.getAcceptedPsychologistByEmail = async function getAcceptedPsychologistByEmail(email) {
   return await knex(module.exports.psychologistsTable)
   .where('state', demarchesSimplifiees.DOSSIER_STATE.accepte)
-  .andWhere('personalEmail', email)
+  .andWhere(
+    knex.raw('LOWER("personalEmail") = ?', email.toLowerCase())
+  )
   .first()
 }
 
