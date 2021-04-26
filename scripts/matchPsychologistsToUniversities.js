63f1bd836077f179ca5b1744dd49dfa2d29056b7
Paul Leclercq <paleclercq@gmail.com>
2021-04-26 16:17:15 +0200

    Refacto/proposal for match psy uni (#158)
    * refacto: use getUniById and ByName
    
    * review: remove some logs

--- scripts/matchPsychologistsToUniversities.js
+++ scripts/matchPsychologistsToUniversities.js
@@ -3,54 +3,33 @@ const dbPsychologists = require('../db/psychologists')
 const demarchesSimplifiees = require('../utils/demarchesSimplifiees')
 const departementToUniversityName = require('./departementToUniversityName')
 
-console.log('departementToUniversityName',
-  Object.entries(departementToUniversityName).length,
-  departementToUniversityName)
+const getUniversityId = function getUniversityId(universities, name) {
+  const foundUni = universities.find ( uni => {
+    return uni.name.toString().trim() === name.toString().trim()
+  })
 
-const makeDepartementToUniversityIdTable = async () => {
-  const universities = await dbUniversities.getUniversities()
-  console.log(universities.length, 'universities', universities)
-  // [{ id: 'my-univ-id', name: 'Avignon'}]
-
-  const universityNameToId = Object.fromEntries(
-    universities.map(university => {
-      return [ university.name, university.id ]
-    })
-  )
-  console.log('universityNameToId', Object.entries(universityNameToId).length, universityNameToId)
-  // { 'Avignon': 'my-univ-id'}
-
-  const universityIdToName = Object.fromEntries(
-    universities.map(university => {
-      return [ university.id, university.name ]
-    })
-  )
-  console.log('universityNameToId', Object.entries(universityNameToId).length, universityNameToId)
-  // { 'my-univ-id': 'Avignon' }
-
-  const departementToId = Object.fromEntries(
-    Object.entries(departementToUniversityName).map(([departement, name]) => {
-      return [ departement, universityNameToId[name]]
-    })
-  )
-  console.log('departementToId', Object.entries(departementToId).length, departementToId)
-  // { '55': 'my-univ-id'}
-
-  return [universityNameToId, universityIdToName, departementToId]
+  return foundUni.id;
+}
+const getUniversityName = function getUniversityName(universities, id) {
+  return universities.filter ( uni => {
+    uni.id === id
+  }).name;
 }
 
 const run = async (withWrite) => {
-  // todo try catch
-  const [universityNameToId, universityIdToName, departementToUnivId] = await makeDepartementToUniversityIdTable()
+  console.log("Match psychologists to universities...")
+  const universities = await dbUniversities.getUniversities()
+  console.log(`${universities.length} universities`)
 
   let psychologists = await dbPsychologists.getPsychologistsDeclaredUniversity()
-  //psychologists = psychologists.slice(0, 10) // todo remove
+  console.log(`${psychologists.length} psychologists`)
 
   const statsNoUniversityFound = {}
   const statsNoDepartementFound = []
   const statsConflictingDeclaredUniversity = {}
   const statsAssignedWithoutProblem = []
   const statsAlreadyAssigned = []
+
   psychologists.forEach(async (psychologist) => {
     if (psychologist.assignedUniversityId) {
       // Don't rewrite assignedUniversityId if it's already written, in case we made changes by hand that should not be
@@ -70,9 +49,10 @@ const run = async (withWrite) => {
       statsNoDepartementFound.push(psychologist.dossierNumber)
       return
     }
-    const universityIdToAssign = departementToUnivId[departement]
+    const universityIdToAssign = getUniversityId(universities, departementToUniversityName[departement])
+
     if (!universityIdToAssign) {
-      console.log(`No university found for departement ${psychologist.departement}
+      console.warn(`No university found for departement ${psychologist.departement}
       - psy id ${psychologist.dossierNumber}
       - ${psychologist.lastName} ${psychologist.firstNames}`)
 
@@ -81,15 +61,18 @@ const run = async (withWrite) => {
       }
       statsNoUniversityFound[psychologist.departement].push(psychologist.dossierNumber)
       return
+    } else {
+      console.log(`will assign ${universityIdToAssign} based on ${psychologist.departement}`);
     }
 
     // If the psychologist declared another university, something is wrong ! Do not write assignedUniversityId.
-    const noUniversityId = universityNameToId['--- Aucune pour le moment']
+    const noUniversityId = getUniversityId(universities, '--- Aucune pour le moment')
     if (psychologist.declaredUniversityId &&
         psychologist.declaredUniversityId !== noUniversityId && // the psy declared "Aucune"
         psychologist.declaredUniversityId !== universityIdToAssign) {
       console.log('Psy', psychologist.dossierNumber, 'already declared', psychologist.declaredUniversityId,
-        'so will not assign', universityIdToAssign, universityIdToName[universityIdToAssign])
+        'so will not assign', universityIdToAssign, getUniversityName(universities, universityIdToAssign))
+
       if (!statsConflictingDeclaredUniversity[psychologist.departement]) {
         statsConflictingDeclaredUniversity[psychologist.departement] = []
       }
@@ -98,9 +81,9 @@ const run = async (withWrite) => {
           psychologistId : psychologist.dossierNumber,
           psychologistEmail: psychologist.personalEmail,
           declaredUniversityId: psychologist.declaredUniversityId,
-          declaredUniversityName: universityIdToName[psychologist.declaredUniversityId],
+          declaredUniversityName: getUniversityName(universities, psychologist.declaredUniversityId),
           universityIdToAssign: universityIdToAssign,
-          universityNameToAssign: universityIdToName[universityIdToAssign]
+          universityNameToAssign: getUniversityName(universities, universityIdToAssign)
         }
       )
       return
@@ -115,7 +98,7 @@ const run = async (withWrite) => {
     }
     console.log(withWrite ? '' : '[NO WRITE]','Assigned', psychologist.dossierNumber,
       'of departement', psychologist.departement,
-      'to university', universityIdToAssign, universityIdToName[universityIdToAssign])
+      'to university', universityIdToAssign, getUniversityName(universities, universityIdToAssign))
     statsAssignedWithoutProblem.push(psychologist.dossierNumber)
   })
 
@@ -132,6 +115,9 @@ const run = async (withWrite) => {
   console.log('\nAssigned without problem :', statsAssignedWithoutProblem.length, 'psys')
 
   console.log('\nAlready assigned, so this script did nothing :', statsAlreadyAssigned.length, 'psys')
+
+  // eslint-disable-next-line no-process-exit
+  process.exit(1)
 }
 
-module.exports.run = run
+run();
