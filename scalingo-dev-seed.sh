42db3b96c0db49dc9db917d703fabebeb6e2b0c6
Paul Leclercq <paleclercq@gmail.com>
2021-04-30 11:49:48 +0200

    fix(ci): remove script execution as it is now executed by npm run seed

--- scalingo-dev-seed.sh
+++ scalingo-dev-seed.sh
@@ -4,8 +4,6 @@ if [ "$SEED_FAKE_DATA_DATABASE" = "true" ] ; then
   echo "Seeding database";
   npm run migrate;
   npm run seed;
-  node scripts/insertUniversities.js; # Insert into universities tables
-  node scripts/insertEmailToUniversities.js TO-DO.csv; # insert emails contacts from CSV files (need to ask support for rights)
 else
   echo "Not seeding database";
 fi
