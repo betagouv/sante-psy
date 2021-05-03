2d53461029f15a42e2a916f4af3af80c5b511757
Paul Leclercq <paleclercq@gmail.com>
2021-05-03 15:26:38 +0200

    Feat(patientFolder): add missing info (#164)
    * wip: add birthday date
    
    * wip: refacto remove doctorPhone, replaced it with birthday
    
    * test: ok
    
    * refacto: birthday --> dateOfBirth
    
    * editPatient: remove title date of birth
    
    * editPatient: change 'title' date of birth input
    
    * refacto: remove momentjs for Date
    
    * refacto: Date instead of moment
    
    * Simplify date field text, and follow recommendations of design system
    
    * log: log level to debug
    
    * New cronjob file WIP
    
    * Making join in appoitments table to get info by university
    
    * Get Psy informations
    
    * Adding test to get count appointment by university
    
    * Debbuging for the tests
    
    * Fix test for the methode getMonthlyAppointmentsSummary
    
    * Fix docstring in appointments.js
    
    * Clean up the import of the psychologists table
    
    * review: remove logs & add missing attribute for unit test
    
    * Updated CronUniversityPayment + tests
    
    * Removed logs
    
    * use orderby without raw
    
    * Removed old function unused
    
    * Removed unused variable
    
    * Clean files
    
    * refacto methode to generate uuid
    
    * Update db/appointments.js
    
    remove todo
    
    Co-authored-by: Paul Leclercq <paleclercq@gmail.com>
    
    * PsyListing : 20 results per page
    
    * Chore/upgrade design system (#142)
    
    * Upgrade design system
    
    * Fix alignment
    
    * WIP
    
    * update: design system - wip: header to do
    
    * wip: everything done, need to review it
    
    * ui: copy color from former theme for success / warning
    
    * ui: update legals page
    
    * review
    
    Co-authored-by: Paul Leclercq <paleclercq@gmail.com>
    
    * review: destructuring date
    
    * feat: mandatory date of birth after config date
    
    * review: remove dead code linter
    
    * feat: add missing info details to patient folder
    
    * fix merge
    
    * refactor(dashboard): update has folder completed
    
    * Remove hasAllPatientsWithCompletedFolders from back computation
    
    Co-authored-by: Estelle Comment <estelle.comment@gmail.com>
    Co-authored-by: eml-trm <termeau.e@gmail.com>
    Co-authored-by: eml-trm <eml-trm@users.noreply.github.com>
    Co-authored-by: Estelle Comment <estelle.comment@beta.gouv.fr>
    Co-authored-by: Sandrine Ricardo <sandrine.ricardo@gmail.com>
    Co-authored-by: Xavierd <xavier.desoindre@hotmail.fr>

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,73 +1,75 @@
-const cookie = require('../utils/cookie')
-const config = require('../utils/config')
-const date = require('../utils/date')
-const dbAppointments = require('../db/appointments')
-const dbPatient = require('../db/patients')
+const cookie = require('../utils/cookie');
+const config = require('../utils/config');
+const date = require('../utils/date');
+const dbAppointments = require('../db/appointments');
+const dbPatient = require('../db/patients');
+
+const DOCTOR_NAME = 'nom du docteur';
+const INSTITUTION_NAME = 'établissement scolaire';
+const DOCTOR_ADDRESS = 'adresse du docteur';
+const BIRTH_DATE = 'date de naissance';
+const STUDENT_STATUS = 'statut étudiant';
+const PRESCRIPTION = 'orientation médicale';
 
 function hasFolderCompleted(patient) {
-  let notEmptyDoctorName = false;
-  let notEmptyInstitutionName = false;
-  let notEmptyDoctorAddress = false;
-  let mandatoryDateOfBirth = false;
+  const missingInfo = [];
 
-  if(patient.doctorName) {
-    notEmptyDoctorName = !(!patient.doctorName.trim())
-  }
-  if(patient.institutionName) {
-    notEmptyInstitutionName = !(!patient.institutionName.trim())
+  if (!patient.doctorName || !patient.doctorName.trim()) {
+    missingInfo.push(DOCTOR_NAME);
   }
 
-  if(patient.doctorAddress) {
-    notEmptyDoctorAddress = !(!patient.doctorAddress.trim())
+  if (!patient.institutionName || !patient.institutionName.trim()) {
+    missingInfo.push(INSTITUTION_NAME);
   }
 
-  if(patient.dateOfBirth) {
-    mandatoryDateOfBirth = true
-  } else if ( patient.createdAt <= date.parseDateForm(config.dateOfBirthDeploymentDate) ) {
-    mandatoryDateOfBirth = true
+  if (!patient.doctorAddress || !patient.doctorAddress.trim()) {
+    missingInfo.push(DOCTOR_ADDRESS);
   }
 
-  return patient.hasPrescription &&
-    patient.isStudentStatusVerified &&
-    notEmptyDoctorName &&
-    notEmptyInstitutionName &&
-    notEmptyDoctorAddress &&
-    mandatoryDateOfBirth
+  if (!patient.dateOfBirth && patient.createdAt > date.parseDateForm(config.dateOfBirthDeploymentDate)) {
+    missingInfo.push(BIRTH_DATE);
+  }
+
+  if (!patient.isStudentStatusVerified) {
+    missingInfo.push(STUDENT_STATUS);
+  }
+
+  if (!patient.hasPrescription) {
+    missingInfo.push(PRESCRIPTION);
+  }
+
+  return {
+    folderCompleted: missingInfo.length === 0,
+    missingInfo: missingInfo.join(', '),
+  };
 }
 
 module.exports.dashboard = async function dashboard(req, res) {
   try {
-    let hasAllPatientsWithCompletedFolders = true;
-    const psychologistId = cookie.getCurrentPsyId(req)
-    const results = await Promise.all([
+    const psychologistId = cookie.getCurrentPsyId(req);
+    const [patients, appointments] = await Promise.all([
       dbPatient.getPatients(psychologistId),
       dbAppointments.getAppointments(psychologistId),
-    ])
-    const patients = results[0]
-    const appointments = results[1]
+    ]);
 
-    // @TODO create a function for this
-    const patientsWithFolderStatus = patients.map ( patient => {
-      patient.hasFolderCompleted = hasFolderCompleted(patient)
-      if(!patient.hasFolderCompleted) {
-        hasAllPatientsWithCompletedFolders = false;
-      }
-      return patient;
+    patients.forEach((patient) => {
+      const { folderCompleted, missingInfo } = hasFolderCompleted(patient);
+      patient.hasFolderCompleted = folderCompleted;
+      patient.missingInfo = missingInfo;
     });
 
     res.render('dashboard', {
-      appointments: appointments,
-      patients: patientsWithFolderStatus,
-      hasAllPatientsWithCompletedFolders: hasAllPatientsWithCompletedFolders,
+      appointments,
+      patients,
       announcement: config.announcement,
       dateOfBirthDeploymentDate: config.dateOfBirthDeploymentDate,
     });
   } catch (err) {
-    req.flash('error', 'Impossible de charger les séances et les patients. Réessayez ultérieurement.')
+    req.flash('error', 'Impossible de charger les séances et les patients. Réessayez ultérieurement.');
     console.error('dashboard', err);
     res.render('dashboard', {
       appointments: [],
-      patients: []
+      patients: [],
     });
   }
-}
+};
--- test/test-dashboardController.js
+++ test/test-dashboardController.js
@@ -1,119 +1,137 @@
 /* eslint-disable func-names */
-const app = require('../index')
-const chai = require('chai')
-const rewire = require('rewire')
-const clean = require('./helper/clean')
-const cookie = require('../utils/cookie')
-const date = require('../utils/date')
-const dashboardController = rewire('../controllers/dashboardController')
-const dbAppointments = require('../db/appointments')
-const dbPatients = require('../db/patients')
-const format = require('../utils/format')
+const app = require('../index');
+const chai = require('chai');
+const rewire = require('rewire');
+const clean = require('./helper/clean');
+const cookie = require('../utils/cookie');
+const date = require('../utils/date');
 
-describe('dashboardController', function() {
-  const dateOfBirth = date.parseDateForm('20/01/1980')
-  describe('hasFolderCompleted', function() {
-    let patient
-    beforeEach( function() {
-      patient =  {
-        firstNames : "firstNames",
-        lastName : "lastName",
-        dateOfBirth : dateOfBirth,
-        INE : "INE",
-        institutionName : "institutionName",
-        isStudentStatusVerified : true,
-        hasPrescription : true,
-        psychologistId : "psychologistId",
-        doctorName : "doctorName",
-        doctorAddress : "30000 Nîmes",
-        createdAt : date.parseDateForm('20/01/2000'),
-      }
-    })
+const dashboardController = rewire('../controllers/dashboardController');
+const dbAppointments = require('../db/appointments');
+const dbPatients = require('../db/patients');
+
+describe.only('dashboardController', () => {
+  const dateOfBirth = date.parseDateForm('20/01/1980');
+  describe('hasFolderCompleted', () => {
+    let patient;
+    beforeEach(() => {
+      patient = {
+        firstNames: 'firstNames',
+        lastName: 'lastName',
+        dateOfBirth,
+        INE: 'INE',
+        institutionName: 'institutionName',
+        isStudentStatusVerified: true,
+        hasPrescription: true,
+        psychologistId: 'psychologistId',
+        doctorName: 'doctorName',
+        doctorAddress: '30000 Nîmes',
+        createdAt: date.parseDateForm('20/01/2000'),
+      };
+    });
 
     const hasFolderCompleted = dashboardController.__get__('hasFolderCompleted');
-    it('should return true if patient has all needed info', function() {
-      hasFolderCompleted(patient).should.equal(true);
+    it('should return true if patient has all needed info', () => {
+      const { folderCompleted, missingInfo } = hasFolderCompleted(patient);
+
+      missingInfo.should.equal('');
+      folderCompleted.should.equal(true);
     });
 
-    it('should return true if INE is missing', function() {
-      patient.INE=null;
-      hasFolderCompleted(patient).should.equal(true);
+    it('should return true if INE is missing', () => {
+      patient.INE = null;
+      const { folderCompleted, missingInfo } = hasFolderCompleted(patient);
+      missingInfo.should.equal('');
+      folderCompleted.should.equal(true);
     });
 
-
-    it('should return true if dateOfBirth is missing', function() {
-      patient.dateOfBirth=null;
-      hasFolderCompleted(patient).should.equal(true);
+    it('should return false if doctor name is missing', () => {
+      patient.doctorName = null;
+      const { folderCompleted, missingInfo } = hasFolderCompleted(patient);
+      missingInfo.should.equal('nom du docteur');
+      folderCompleted.should.equal(false);
     });
 
-    it('should return false if doctor name is missing', function() {
-      patient.doctorName=null;
-      hasFolderCompleted(patient).should.equal(false);
+    it('should return false if isStudentStatusVerified is false', () => {
+      patient.isStudentStatusVerified = false;
+      const { folderCompleted, missingInfo } = hasFolderCompleted(patient);
+      missingInfo.should.equal('statut étudiant');
+      folderCompleted.should.equal(false);
     });
 
-    it('should return false if isStudentStatusVerified is false', function() {
-      patient.isStudentStatusVerified=false;
-      hasFolderCompleted(patient).should.equal(false);
+    it('should return false if hasPrescription is false', () => {
+      patient.hasPrescription = false;
+      const { folderCompleted, missingInfo } = hasFolderCompleted(patient);
+      missingInfo.should.equal('orientation médicale');
+      folderCompleted.should.equal(false);
     });
 
-    it('should return false if hasPrescription is false', function() {
-      patient.hasPrescription=false;
-      hasFolderCompleted(patient).should.equal(false);
+    it('should return false if doctorName is a only white spaces', () => {
+      patient.doctorName = '    ';// trim should work
+      const { folderCompleted, missingInfo } = hasFolderCompleted(patient);
+      missingInfo.should.equal('nom du docteur');
+      folderCompleted.should.equal(false);
+    });
+    it('should return false if doctorName is an empty string', () => {
+      patient.doctorName = '';
+      const { folderCompleted, missingInfo } = hasFolderCompleted(patient);
+      missingInfo.should.equal('nom du docteur');
+      folderCompleted.should.equal(false);
     });
 
-    it('should return false if doctorName is a only white spaces', function() {
-      patient.doctorName="    ", // trim should work
-      hasFolderCompleted(patient).should.equal(false);
-    });
-    it('should return false if doctorName is an empty string', function() {
-      patient.doctorName="";
-      hasFolderCompleted(patient).should.equal(false);
+    it('should return false if institutionName is a only white spaces', () => {
+      patient.institutionName = '    ';
+      const { folderCompleted, missingInfo } = hasFolderCompleted(patient);
+      missingInfo.should.equal('établissement scolaire');
+      folderCompleted.should.equal(false);
     });
 
-    it('should return false if institutionName is a only white spaces', function() {
-      patient.institutionName='    ';
-      hasFolderCompleted(patient).should.equal(false);
+    it('should return false if doctorAddress is an empty string', () => {
+      patient.doctorAddress = '';
+      const { folderCompleted, missingInfo } = hasFolderCompleted(patient);
+      missingInfo.should.equal('adresse du docteur');
+      folderCompleted.should.equal(false);
+    });
+    it('should return false if doctor adress is a only white spaces', () => {
+      patient.doctorAddress = '    ';
+      const { folderCompleted, missingInfo } = hasFolderCompleted(patient);
+      missingInfo.should.equal('adresse du docteur');
+      folderCompleted.should.equal(false);
     });
 
-    it('should return false if doctorAddress is an empty string', function() {
-      patient.doctorAddress='';
-      hasFolderCompleted(patient).should.equal(false);
-    });
-    it('should return false if doctor adress is a only white spaces', function() {
-      patient.doctorAddress='    ';
-      hasFolderCompleted(patient).should.equal(false);
+    it('should return true if missing date of birth with createdAt before deployement feature date', () => {
+      patient.dateOfBirth = null;
+      const { folderCompleted, missingInfo } = hasFolderCompleted(patient);
+      missingInfo.should.equal('');
+      folderCompleted.should.equal(true);
     });
 
-    it('should return true if missing date of birth before createdAt deployement feature date', function() {
-      patient.dateOfBirth=null,
-      hasFolderCompleted(patient).should.equal(true);
+    it('should return false if missing date of birth with createdAt after deployement feature date', () => {
+      patient.dateOfBirth = null;
+      patient.createdAt = date.parseDateForm('20/05/2021');
+      const { folderCompleted, missingInfo } = hasFolderCompleted(patient);
+      missingInfo.should.equal('date de naissance');
+      folderCompleted.should.equal(false);
     });
-
-    it('should return false if missing date of birth before createdAt deployement feature date', function() {
-      patient.dateOfBirth= null
-      patient.createdAt= date.parseDateForm('20/05/2021')
-      hasFolderCompleted(patient).should.equal(false);
-    });
-
   });
-  
-  describe('display dashaboard', function() {
-    beforeEach(async function(done) {
-      done()
-    })
 
-    afterEach(async function() {
-      await clean.cleanAllPatients()
-      await clean.cleanAllAppointments()
-      return Promise.resolve()
-    })
+  describe('display dashboard', () => {
+    beforeEach(async (done) => {
+      done();
+    });
 
-    it('should display only my patients in dashboard', async function() {
+    afterEach(async () => {
+      await clean.cleanAllPatients();
+      await clean.cleanAllAppointments();
+      return Promise.resolve();
+    });
+
+    it('should display only my patients in dashboard', async () => {
       const psy = {
         dossierNumber: '9a42d12f-8328-4545-8da3-11250f876146',
         email: 'prenom.nom@beta.gouv.fr',
-      }
-      const anotherPsyId = '60014566-d8bf-4f01-94bf-27b31ca9275d'
+      };
+      const anotherPsyId = '60014566-d8bf-4f01-94bf-27b31ca9275d';
       const myPatient = await dbPatients.insertPatient(
         'Ada',
         'Lovelace',
@@ -125,7 +143,7 @@ describe('dashboardController', function() {
         'Dr Docteur',
         'adresse du docteur',
         dateOfBirth,
-      )
+      );
       const patientForAnotherPsy = await dbPatients.insertPatient(
         'Stevie',
         'Wonder',
@@ -137,7 +155,7 @@ describe('dashboardController', function() {
         'Dr Docteur',
         'adresse du docteur',
         dateOfBirth,
-      )
+      );
 
       return chai.request(app)
         .get('/psychologue/mes-seances')
@@ -145,22 +163,22 @@ describe('dashboardController', function() {
         .redirects(0) // block redirects, we don't want to test them
         .then(async (res) => {
           // My patients are present
-          chai.assert.include(res.text, myPatient.firstNames)
-          chai.assert.include(res.text, myPatient.lastName)
+          chai.assert.include(res.text, myPatient.firstNames);
+          chai.assert.include(res.text, myPatient.lastName);
 
           // Other psy's patients are not listed
-          chai.assert.notInclude(res.text, patientForAnotherPsy.firstNames)
-          chai.assert.notInclude(res.text, patientForAnotherPsy.lastName)
-          return Promise.resolve()
-        })
-    })
+          chai.assert.notInclude(res.text, patientForAnotherPsy.firstNames);
+          chai.assert.notInclude(res.text, patientForAnotherPsy.lastName);
+          return Promise.resolve();
+        });
+    });
 
-    it('should display only my appointments in dashboard', async function() {
+    it('should display only my appointments in dashboard', async () => {
       const psy = {
         dossierNumber: '9a42d12f-8328-4545-8da3-11250f876146',
         email: 'prenom.nom@beta.gouv.fr',
-      }
-      const anotherPsyId = '60014566-d8bf-4f01-94bf-27b31ca9275d'
+      };
+      const anotherPsyId = '60014566-d8bf-4f01-94bf-27b31ca9275d';
       const myPatient = await dbPatients.insertPatient(
         'Ada',
         'Lovelace',
@@ -172,7 +190,7 @@ describe('dashboardController', function() {
         'Dr Docteur',
         'adresse du docteur',
         dateOfBirth,
-      )
+      );
       const patientForAnotherPsy = await dbPatients.insertPatient(
         'Stevie',
         'Wonder',
@@ -184,11 +202,17 @@ describe('dashboardController', function() {
         'Dr Docteur',
         'adresse du docteur',
         dateOfBirth,
-      )
-      const myAppointment =
-        await dbAppointments.insertAppointment(new Date('2021-03-01'), myPatient.id, psy.dossierNumber)
-      const appointmentForAnotherPsy =
-        await dbAppointments.insertAppointment(new Date('2021-12-12'), patientForAnotherPsy.id, anotherPsyId)
+      );
+      const myAppointment = await dbAppointments.insertAppointment(
+        new Date('2021-03-01'),
+        myPatient.id,
+        psy.dossierNumber
+      );
+      const appointmentForAnotherPsy = await dbAppointments.insertAppointment(
+        new Date('2021-12-12'),
+        patientForAnotherPsy.id,
+        anotherPsyId
+      );
 
       return chai.request(app)
         .get('/psychologue/mes-seances')
@@ -196,14 +220,13 @@ describe('dashboardController', function() {
         .redirects(0) // block redirects, we don't want to test them
         .then(async (res) => {
           // My appointments are present
-          chai.assert.include(res.text, date.formatFrenchDate(myAppointment.appointmentDate))
+          chai.assert.include(res.text, date.formatFrenchDate(myAppointment.appointmentDate));
 
           // Other psy's patients are not listed
-          chai.assert.notInclude(res.text, date.formatFrenchDate(appointmentForAnotherPsy.appointmentDate))
+          chai.assert.notInclude(res.text, date.formatFrenchDate(appointmentForAnotherPsy.appointmentDate));
 
-          return Promise.resolve()
-        })
-    })
-
-  })
-})
+          return Promise.resolve();
+        });
+    });
+  });
+});
