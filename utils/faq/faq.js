1bce2704b1a9423899d751ab45f118f4fb7e6340
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2021-05-04 18:15:21 +0200

    Separate faq in a proper service (#179)

---
+++ utils/faq/faq.js
@@ -0,0 +1,13 @@
+const students = require('./students');
+const eligibility = require('./eligibility');
+const process = require('./process');
+const reimbursement = require('./reimbursement');
+const doctor = require('./doctor');
+
+module.exports = {
+  students,
+  eligibility,
+  process,
+  reimbursement,
+  doctor,
+};
