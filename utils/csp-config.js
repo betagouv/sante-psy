4246ba739b98d9f81869c5bd0eebcfd6b51f76e1
Estelle Comment <estelle.comment@gmail.com>
2021-05-18 09:54:51 +0200

    Add hotjar

--- utils/csp-config.js
+++ utils/csp-config.js
@@ -3,7 +3,9 @@ const helmet = require('helmet');
 module.exports = helmet.contentSecurityPolicy({
   directives: {
     ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-    'script-src': ["'self'", 'https://stats.data.gouv.fr/'],
+    'script-src': ["'self'", 'https://stats.data.gouv.fr/', 'https://*.hotjar.com'],
     'img-src': ["'self'", 'https://stats.data.gouv.fr/', 'data:'],
+    'frame-src': ['https://*.hotjar.com'],
+    'connect-src': ['https://*.hotjar.com'],
   },
 });
