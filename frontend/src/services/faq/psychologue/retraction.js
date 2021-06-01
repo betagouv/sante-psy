9d95fb951957b1c86f8b43cac42e4ffdfe9e0d51
Damir <damir.sagadbekov@live.fr>
2021-06-01 17:40:48 +0200

    config.contactEmail

--- frontend/src/services/faq/psychologue/retraction.js
+++ frontend/src/services/faq/psychologue/retraction.js
@@ -4,7 +4,7 @@ module.exports = config => [
   {
     question: 'Je souhaite être retiré du dispositif, comment faire ?',
     answer: `Pour être retiré du dispositif Santé Psy Étudiant, envoyez-nous un email \
-    à <a href="mailto:${config.contactEmail}">support-santepsyetudiant@beta.gouv.fr</a> \
+    à <a href="mailto:${config.contactEmail}">${config.contactEmail}</a> \
     En nous indiquant votre volonté d'être retiré du dispositif et en précisant si vous avez déjà effectué des séances.`,
   },
 ];
