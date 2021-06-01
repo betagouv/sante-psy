d0148cfac887bf02512a0025dc1a3d8e917e69a1
Damir <damir.sagadbekov@live.fr>
2021-06-01 15:10:12 +0200

    config

--- frontend/src/services/faq/psychologue/connection.js
+++ frontend/src/services/faq/psychologue/connection.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-multi-str */
 /* eslint-disable max-len */
-module.exports = () => [
+module.exports = config => [
   {
     question: "Je n'arrive pas à entrer mes séances sur la plateforme. Comment faire ?",
-    answer: 'Selon votre demande, assurez-vous de bien lire tous les onglets pour déclarer vos séances. \
-    Une fois cela établi, si votre problème persiste, envoyez-nous un email à <a href="mailto:support-santepsyetudiant@beta.gouv.fr">support-santepsyetudiant@beta.gouv.fr</a>',
+    answer: `Selon votre demande, assurez-vous de bien lire tous les onglets pour déclarer vos séances. \
+    Une fois cela établi, si votre problème persiste, envoyez-nous un email à <a href="mailto:${config.contactEmail}">support-santepsyetudiant@beta.gouv.fr</a>`,
   },
   {
     question: 'Je souhaite modifier des informations sur mon espace. Comment puis-je faire ?',
-    answer: 'Pour modifier vos informations, envoyez-nous un email à <a href="mailto:support-santepsyetudiant@beta.gouv.fr">support-santepsyetudiant@beta.gouv.fr</a> \
+    answer: `Pour modifier vos informations, envoyez-nous un email à <a href="mailto:${config.contactEmail}">support-santepsyetudiant@beta.gouv.fr</a> \
     pour repasser votre dossier "en construction". Sachez toutefois que pendant ce temps, \
-    vous n’apparaîtrez plus sur l\'annuaire, et vous ne pourrez pas déclarer vos séances. Une fois les modifications effectuées,nous vous invitons à nous renvoyer un email de confirmation afin de réactiver votre dossier.',
+    vous n’apparaîtrez plus sur l\'annuaire, et vous ne pourrez pas déclarer vos séances. Une fois les modifications effectuées,nous vous invitons à nous renvoyer un email de confirmation afin de réactiver votre dossier.`,
   },
   {
     question: 'Je ne parviens pas à accéder à mon espace psychologue. Que puis-je faire ?',
