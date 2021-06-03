58e15b1c443b05c414c319272279b59754dcbdd5
LinaCHAM <85121719+LinaCHAM@users.noreply.github.com>
2021-06-03 17:21:45 +0200

    modif/faq/psy/process

--- frontend/src/services/faq/psychologue/process.js
+++ frontend/src/services/faq/psychologue/process.js
@@ -2,7 +2,7 @@
 /* eslint-disable max-len */
 module.exports = config => [
   {
-    question: 'Je suis déjà des étudiants. Puis-je continuer à les suivre dans le cadre du dispositif Santé Psy Etudiants ?',
+    question: 'Je suis déjà des étudiants. Puis-je continuer à les suivre dans le cadre du dispositif Santé Psy Étudiant ?',
     answer: `Si les étudiants que vous prenez en charge actuellement souhaitent bénéficier du dispositif et \
     poursuivre leur consultation avec vous, il est nécessaire qu’ils soient orientés vers un psychologue par le SSU dont \
     ils dépendent ou un médecin généraliste. Parallèlement, pour pouvoir continuer à suivre ces étudiants, \
@@ -12,19 +12,19 @@ module.exports = config => [
     mise à disposition des étudiants et médecins généralistes.`,
   },
   {
-    question: 'Les étudiants peuvent-ils directement venir voir le/la psy ?',
-    answer: 'Dans le cadre du dispositif Santé Psy Etudiants, les étudiants doivent consulter le service de santé universitaire ou \
+    question: 'Les étudiants peuvent-ils directement venir voir le psychologue ?',
+    answer: 'Dans le cadre du dispositif Santé Psy Étudiant, les étudiants doivent consulter le service de santé universitaire ou \
     un médecin généraliste qui peut les orienter vers un psychologue partenaire de la démarche.',
   },
   {
     question: 'Comment un étudiant sait s’il a un SSU ? Comment fait-il s’il n’en a pas ?',
     answer: 'Les étudiants peuvent savoir si leur établissement dispose d’un service de santé universitaire (SSU) ou s’ils ont accès au SSU \
-    d’un autre établissement (par convention) en consultant le site de leur université ou école ou en leur téléphonant. \
+    d’un autre établissement (par convention) en consultant le site de leur université / école ou en leur téléphonant. \
     Si l’étudiant n’a pas accès à un SSU, il peut se rendre chez un médecin généraliste qui l’orientera en cas de besoin vers un psychologue partenaire du dispositif.\
     D’une façon générale, les établissements disposant d’un SSU sont invités à ouvrir autant que possible leur service aux écoles partenaires ou partageant le site.',
   },
   {
-    question: 'Qui doit renouveler les séances ? Faut-il vraiment repasser chez le médecin ?',
+    question: 'Qui doit renouveler les séances ? Faut-il nécessairement repasser chez le médecin ?',
     answer: 'Au-delà d’un premier cycle de trois séances, l’étudiant peut être de nouveau orienté, \
     obligatoirement par le médecin du service de santé universitaire ou le médecin généraliste, \
     vers un psychologue pour un second cycle de trois séances au plus. \
@@ -41,13 +41,13 @@ module.exports = config => [
   {
     question: 'Il est indiqué que les psychologues feront acte de candidature via un formulaire simplifié en ligne et \
     qu’une plateforme numérique permettra de déclarer le nombre de consultations effectuées. Serait-il possible d’y avoir accès ou bien est-ce réservé aux SSU ?',
-    answer: 'Ce sera au psychologue de faire la déclaration des séances faites une fois qu’il est inscrit via le formulaire en ligne et \
-    que son dossier est accepté.  Cette déclaration se fera sur la plateforme santé psy étudiant, dans l’espace personnel du psychologue lorsque que la plateforme sera accessible.',
+    answer: 'C'est au psychologue de faire la déclaration des séances faites une fois qu’il est inscrit via le formulaire en ligne et \
+    que son dossier est accepté.  Cette déclaration se fait sur la plateforme Santé Psy Étudiant, dans l’espace personnel du psychologue une fois le dossier accepté.',
   },
   {
-    question: "Je reçois beaucoup de demande d'étudiants, je souhaite ne plus être affiché dans la liste publique des partenaires psychologues, \
+    question: "Je reçois beaucoup de demande d'étudiants, je souhaite ne plus être affiché dans la liste publique des psychologues partenaires, \
     tout en restant dans le dispositif, est-ce possible ?",
     answer: 'Vous avez la possibilité de vous inscrire sur la plateforme et ainsi faire bénéficier les étudiants de séances dans le cadre du dispositif. \
-    Dans le cas d’une forte demande des étudiants, vous avez la possibilité de refuser par manque de disponibilités lorsqu’un étudiant vous contacte ou éventuellement de l’orienter vers un(e) autre psychologue du dispositif.',
+    Dans le cas d’une forte demande des étudiants, vous avez la possibilité de refuser par manque de disponibilités lorsqu’un étudiant vous contacte ou éventuellement de l’orienter vers un autre psychologue du dispositif.',
   },
 ];
