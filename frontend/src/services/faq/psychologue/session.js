951aaf4314a78f3ad0a393b568159c4bf86f0fa5
LinaCHAM <85121719+LinaCHAM@users.noreply.github.com>
2021-06-04 09:49:57 +0200

    modifs/faq/session/psy (#229)
    * modifs/faq/session/psy
    
    * Update session.js
    
    * Update session.js
    
    Co-authored-by: Xavier Desoindre <xavier.desoindre@hotmail.fr>

--- frontend/src/services/faq/psychologue/session.js
+++ frontend/src/services/faq/psychologue/session.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-multi-str */
 /* eslint-disable max-len */
-module.exports = () => [
+module.exports = config => [
   {
     question: "Puis-je recevoir des étudiants d'autres universités et/ou de départements ?",
     answer: "Oui, vous avez la possibilité de prendre des étudiants quel que soit \
@@ -10,27 +10,27 @@ module.exports = () => [
   {
     question: "Mon patient étudiant n'a pas de numéro INE, que faire ?",
     answer: "Le numéro INE est facultatif. \
-    L'étudiant doit seulement vous présenter sa carte étudiante et sa lettre d'orientation de son médecin généraliste.",
+    L'étudiant doit seulement vous présenter sa carte d'étudiant et sa lettre d'orientation de son médecin généraliste.",
   },
   {
-    question: 'Qu\'est ce que les "chèques psy" (aussi appelé pass santé psy) ?',
+    question: 'Qu\'est ce que les "chèques psy" (aussi appelé "pass santé psy") ?',
     answer: 'Le terme "chèque psy" est employé par la ministre de l\'Enseignement Supérieur et de la Recherche. \
     Il indique l\'accès aux 3 séances renouvelable 1 fois par la lettre d\'orientation du médecin généraliste.',
   },
   {
     question: 'Je reçois trop de demande, comment faire ?',
-    answer: "Vous êtes en droit de refuser des étudiants si vous ne pouvez pas répondre à toutes les demandes. \
+    answer: `Vous êtes en droit de refuser des étudiants si vous ne pouvez pas répondre à toutes les demandes. \
     Vous pouvez également être temporairement retiré de l'annuaire publique des psychologues partenaires. \
-    Pour cela, envoyez nous un email à support-santepsyetudiant@beta.gouv.fr nous le précisant.",
+    Pour cela, envoyez nous un email à <a href="mailto:${config.contactEmail}">${config.contactEmail}</a> nous le précisant.`,
   },
   {
     question: 'Quelles informations dois-je vérifier auprès du patient lors de la séance ?',
-    answer: 'Lors de chaque séance, vous devez renseigner les identités du médecin adresseur et de l’étudiant ainsi que confirmer que la séance a eu lieu. \
+    answer: 'Lors de chaque séance, vous devez renseigner les identités du médecin adresseur et de l’étudiant tout en confirmant que la séance a eu lieu. \
     Cette information permet au SSU de suivre la situation de l’étudiant concerné et à l’administration de l’établissement \
-    (qui n’a pas accès aux informations concernant l’étudiant) de s’assurer du " service fait qui permet de déclencher le paiement.',
+    (qui n’a pas accès aux informations concernant l’étudiant) de s’assurer du service fait qui permet de déclencher le paiement.',
   },
   {
-    question: "Mon patient étudiant n'a pas de carte étudiante. Que puis-je faire ?",
+    question: "Mon patient étudiant n'a pas de carte d'étudiant. Que puis-je faire ?",
     answer: "Les étudiants doivent présenter un justificatif d'inscription en études supérieures : carte étudiante, certificat de scolarité ou autre document.",
   },
 ];
