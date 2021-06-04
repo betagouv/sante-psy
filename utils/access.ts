a0f90afbc2c1d0cb08ff9349b76d7daf9facb4e7
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2021-06-04 10:42:44 +0200

    refactor: move custom types in @types

--- utils/access.ts
+++ utils/access.ts
@@ -1,12 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
-interface CustomRequest extends Request {
-  user: {
-    psychologist: string
-  }
-}
-
-const checkPsyParam = (req: CustomRequest, res: Response, next: NextFunction) : Promise<void> => {
+const checkPsyParam = (req: Request, res: Response, next: NextFunction) : Promise<void> => {
   if (req.params.psyId !== req.user.psychologist) {
     res.status(403).send();
     return;
