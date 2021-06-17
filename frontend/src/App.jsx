2e09a8efeca1b787b200a53adaed525d81131e70
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2021-06-17 16:09:29 +0200

    Improve bundle sizes (#257)
    * Lazy load psy listing page
    
    * Lazy load psychologist part
    
    * compress bundles
    
    * Fix tests

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,8 @@ import ScrollToTop from 'components/ScrollToTop/ScrollToTop';
 import Header from 'components/Header/Header';
 import Landing from 'components/Landing/Landing';
 import Footer from 'components/Footer/Footer';
-import FindPsychologist from 'components/PsyListing/PsyListing';
+
 import Login from 'components/Login/Login';
-import Appointments from 'components/Psychologist/Appointments/Appointments';
-import NewAppointment from 'components/Psychologist/Appointments/NewAppointment';
-import Reimbursement from 'components/Psychologist/Reimbursement/Reimbursement';
-import Patients from 'components/Psychologist/Patients/Patients';
-import AddEditPatient from 'components/Psychologist/Patients/AddEditPatient';
-import PsyProfile from 'components/Psychologist/Profile/PsyProfile';
 import Announcement from 'components/Notification/Announcement';
 import Faq from 'components/Faq/Faq';
 import LegalNotice from 'components/LegalNotice/LegalNotice';
@@ -28,6 +22,9 @@ import '@gouvfr/dsfr/dist/css/dsfr.css';
 
 // Need to be after dsfr to overwrite it
 import './App.css';
+
+const FindPsychologist = React.lazy(() => import('components/PsyListing/PsyListing'));
+const PsychologistRouter = React.lazy(() => import('./PsychologistRouter'));
 
 function App() {
   const { commonStore: { setConfig, config }, userStore: { user, token, pullUser } } = useStore();
@@ -51,30 +48,24 @@ function App() {
       <ScrollToTop />
       {user && <Announcement />}
       {!loading && (
-        <Switch>
-          {user && [
-            <Route key="appointments" exact path="/psychologue/mes-seances" component={Appointments} />,
-            <Route key="new-appointments" exact path="/psychologue/nouvelle-seance" component={NewAppointment} />,
-            <Route key="patients" exact path="/psychologue/mes-patients" component={Patients} />,
-            <Route key="new-patient" exact path="/psychologue/nouveau-patient" component={AddEditPatient} />,
-            <Route key="edit-patient" exact path="/psychologue/modifier-patient/:patientId" component={AddEditPatient} />,
-            <Route key="reimbursment" exact path="/psychologue/mes-remboursements" component={Reimbursement} />,
-            <Route key="psy-profil" path="/psychologue/mon-profil" component={PsyProfile} />,
-          ]}
-          <Route exact path="/psychologue/login/:token?" component={Login} />
-          <Route exact path="/trouver-un-psychologue" component={FindPsychologist} />
-          <Route exact path="/mentions-legales" component={LegalNotice} />
-          <Route exact path="/donnees-personnelles-et-gestion-des-cookies" component={PersonalData} />
-          <Route exact path="/faq" component={Faq} />
-          <Route exact path="/stats" component={Statistics} />
-          <Route exact path="/" component={Landing} />
-          <Route path="/psychologue/">
-            <Redirect to={user ? '/psychologue/mes-seances' : '/psychologue/login'} />
-          </Route>
-          <Route path="/">
-            <Redirect to={user ? '/psychologue/mes-seances' : '/'} />
-          </Route>
-        </Switch>
+        <React.Suspense fallback={<></>}>
+          <Switch>
+            <Route exact path="/psychologue/login/:token?" component={Login} />
+            {user && <Route path="/psychologue/" component={PsychologistRouter} />}
+            <Route exact path="/trouver-un-psychologue" component={FindPsychologist} />
+            <Route exact path="/mentions-legales" component={LegalNotice} />
+            <Route exact path="/donnees-personnelles-et-gestion-des-cookies" component={PersonalData} />
+            <Route exact path="/faq" component={Faq} />
+            <Route exact path="/stats" component={Statistics} />
+            <Route exact path="/" component={Landing} />
+            <Route path="/psychologue/">
+              <Redirect to="/psychologue/login" />
+            </Route>
+            <Route path="/">
+              <Redirect to={user ? '/psychologue/mes-seances' : '/'} />
+            </Route>
+          </Switch>
+        </React.Suspense>
       )}
       <Footer />
     </BrowserRouter>
