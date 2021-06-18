99b3371ec80bc1dc4d856aa20b29e5d9442c2f79
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2021-06-18 15:03:36 +0200

    Only display announcement on psy pages (#264)

--- frontend/src/PsychologistRouter.jsx
+++ frontend/src/PsychologistRouter.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
+
+import Announcement from 'components/Notification/Announcement';
 import Appointments from 'components/Psychologist/Appointments/Appointments';
 import NewAppointment from 'components/Psychologist/Appointments/NewAppointment';
 import Reimbursement from 'components/Psychologist/Reimbursement/Reimbursement';
@@ -8,18 +10,21 @@ import AddEditPatient from 'components/Psychologist/Patients/AddEditPatient';
 import PsyProfile from 'components/Psychologist/Profile/PsyProfile';
 
 const PsychologistRouter = () => (
-  <Switch>
-    <Route exact path="/psychologue/mes-seances" component={Appointments} />
-    <Route exact path="/psychologue/nouvelle-seance" component={NewAppointment} />
-    <Route exact path="/psychologue/mes-patients" component={Patients} />
-    <Route exact path="/psychologue/nouveau-patient" component={AddEditPatient} />
-    <Route exact path="/psychologue/modifier-patient/:patientId" component={AddEditPatient} />
-    <Route exact path="/psychologue/mes-remboursements" component={Reimbursement} />
-    <Route path="/psychologue/mon-profil" component={PsyProfile} />
-    <Route path="/psychologue/">
-      <Redirect to="/psychologue/mes-seances" />
-    </Route>
-  </Switch>
+  <>
+    <Announcement />
+    <Switch>
+      <Route exact path="/psychologue/mes-seances" component={Appointments} />
+      <Route exact path="/psychologue/nouvelle-seance" component={NewAppointment} />
+      <Route exact path="/psychologue/mes-patients" component={Patients} />
+      <Route exact path="/psychologue/nouveau-patient" component={AddEditPatient} />
+      <Route exact path="/psychologue/modifier-patient/:patientId" component={AddEditPatient} />
+      <Route exact path="/psychologue/mes-remboursements" component={Reimbursement} />
+      <Route path="/psychologue/mon-profil" component={PsyProfile} />
+      <Route path="/psychologue/">
+        <Redirect to="/psychologue/mes-seances" />
+      </Route>
+    </Switch>
+  </>
 );
 
 export default PsychologistRouter;
