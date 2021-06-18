90f51cb7965d2d58fa4828a141631ddc3a087346
Xavierd <xavier.desoindre@hotmail.fr>
2021-06-18 13:41:28 +0200

    Manage accent in string comparison

--- utils/string.ts
+++ utils/string.ts
@@ -1,4 +1,10 @@
-const simplify = (value: string) : string => value.replace(/[ -]/g, '').toLowerCase();
+const simplify = (value: string) : string => value
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
+  .normalize('NFD')
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Unicode_Property_Escapes
+  .replace(/\p{Diacritic}/gu, '')
+  .replace(/[ -]/g, '')
+  .toLowerCase();
 
 const areSimilar = (value1: string, value2: string): boolean => simplify(value1) === simplify(value2);
 
