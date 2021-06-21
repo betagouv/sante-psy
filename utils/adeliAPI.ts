400f57f980cd23dae4f185f1a0f9a95812f04d37
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2021-06-21 15:31:18 +0200

    fix: preprocess adeli number in auto verify job

--- utils/adeliAPI.ts
+++ utils/adeliAPI.ts
@@ -6,9 +6,14 @@ const client = axios.create({
   baseURL: 'https://api-sante.herokuapp.com/annuaire',
 });
 
+const preprocessIds = (ids: string[]) : string[] => ids.map((id) => id.replace(/ /g, ''))
+  .filter((id) => id.match(/^\d{9}$/))
+  .map((id) => `0${id}`);
+
 // Note that we add a 0 because we want a adeli number !
 const getAdeliInfo = (ids: string[]) : Promise<{[key: string]: AdeliInfo}> => {
-  const url = `annuaire.json?Identification+nationale+PP__in=${ids.map((id) => `0${id}`).join('%2C')}`;
+  const validIds = preprocessIds(ids);
+  const url = `annuaire.json?Identification+nationale+PP__in=${validIds.join('%2C')}`;
   console.debug(url);
   return client.get(url).then((response) => {
     const { columns } = response.data;
@@ -26,6 +31,7 @@ const getAdeliInfo = (ids: string[]) : Promise<{[key: string]: AdeliInfo}> => {
   });
 };
 
-export default {
+export {
   getAdeliInfo,
+  preprocessIds,
 };
