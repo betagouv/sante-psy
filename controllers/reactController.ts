b114c8816667057c540005df212b2abe1dd41e85
Xavierd <xavier.desoindre@hotmail.fr>
2021-06-25 16:15:07 +0200

    Size of image

--- controllers/reactController.ts
+++ controllers/reactController.ts
@@ -6,7 +6,7 @@ const getIndex = (req: Request, res: Response): void => {
   res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.header('Pragma', 'no-cache');
   res.header('Expires', '0');
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
 };
 
 export default getIndex;
