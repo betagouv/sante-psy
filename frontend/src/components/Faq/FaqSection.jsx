c8ea204af8b5687d923c4b5deeeb8e4fdf1fa7cf
Xavierd <xavier.desoindre@hotmail.fr>
2021-06-29 13:48:15 +0200

    Use Accordion

--- frontend/src/components/Faq/FaqSection.jsx
+++ frontend/src/components/Faq/FaqSection.jsx
@@ -1,42 +1,64 @@
 import React from 'react';
+import { observer } from 'mobx-react';
+import { Accordion, AccordionItem } from '@dataesr/react-dsfr';
+import sanitizeHtml from 'sanitize-html';
 
-import FaqItems from './FaqItems';
+import faq from 'services/faq/faq';
+import { useStore } from 'stores/';
 
-const FaqSection = ({ id, sections, title, flyers }) => (
-  <>
-    <h1 id={id}>{title}</h1>
+const FaqSection = ({ id, sections, title, flyers }) => {
+  const { commonStore: { config } } = useStore();
+  return (
+    <>
+      <h1 id={id}>{title}</h1>
 
-    <div className="fr-mb-3w">
-      <div>
-        <span className="fr-fi-arrow-right-line fr-fi--md" />
-        <span>Vous voulez plus d&lsquo;infos sur le dispositif ?</span>
+      <div className="fr-mb-3w">
+        <div>
+          <span className="fr-fi-arrow-right-line fr-fi--md" />
+          <span>Vous voulez plus d&lsquo;infos sur le dispositif ?</span>
+        </div>
+        <div>
+          {flyers.map(flyer => (
+            <a
+              key={flyer.href}
+              className="fr-btn fr-btn--alt fr-mt-2w fr-mr-2w"
+              href={flyer.href}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {flyer.title}
+            </a>
+          ))}
+        </div>
+        <div className="fr-text--xs fr-mt-2w">
+          Note : si vous utilisez Safari et que le flyer ne s&lsquo;affiche pas (&quot;Module bloqué&quot;),
+          essayez Firefox ou Chrome.
+        </div>
       </div>
-      <div>
-        {flyers.map(flyer => (
-          <a
-            key={flyer.href}
-            className="fr-btn fr-btn--alt fr-mt-2w fr-mr-2w"
-            href={flyer.href}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {flyer.title}
-          </a>
-        ))}
-      </div>
-      <div className="fr-text--xs fr-mt-2w">
-        Note : si vous utilisez Safari et que le flyer ne s&lsquo;affiche pas (&quot;Module bloqué&quot;),
-        essayez Firefox ou Chrome.
-      </div>
-    </div>
 
-    {sections.map(section => (
-      <ul key={section.name} className="fr-accordion-group">
-        {section.title && <h2 id={section.id}>{section.title}</h2>}
-        <FaqItems section={section.name} />
-      </ul>
-    ))}
-  </>
-);
+      {sections.map(section => (
+        <>
+          {section.title && <h2 id={section.id}>{section.title}</h2>}
+          <Accordion key={section.name}>
+            {faq[section.name](config).map(item => (
+              <AccordionItem key={item.question} title={item.question}>
+                <div
+                  // eslint-disable-next-line react/no-danger
+                  dangerouslySetInnerHTML={{
+                    __html: sanitizeHtml(item.answer,
+                      {
+                        allowedTags: ['a'],
+                        allowedAttributes: { a: ['href', 'target', 'rel'] },
+                      }),
+                  }}
+                />
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </>
+      ))}
+    </>
+  );
+};
 
-export default FaqSection;
+export default observer(FaqSection);
