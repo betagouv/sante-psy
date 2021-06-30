cd2b135637d0e5e549802c106401de8852897a62
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2021-06-30 17:57:15 +0200

    fix: add footer links

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import {
   Footer as DSFooter,
   FooterBody,
+  FooterBodyItem,
   FooterBottom,
   FooterLink,
   Logo,
   Link,
 } from '@dataesr/react-dsfr';
 
-const footerLinks = [
+const footerBodyLinks = [
+  { title: 'Contactez-nous', link: '/mentions-legales' },
+  { title: 'etudiant.gouv.fr', link: 'https://www.etudiant.gouv.fr/' },
+  { title: 'Enseignement supérieur', link: 'https://www.enseignementsup-recherche.gouv.fr/' },
+  { title: 'beta.gouv.fr', link: 'https://beta.gouv.fr/' },
+];
+
+const footerBottomLinks = [
   { title: 'Accessibilité: non conforme', link: '/mentions-legales#accessibilite' },
   { title: 'Données personnelles et gestion des cookies', link: '/donnees-personnelles-et-gestion-des-cookies' },
   { title: 'Mentions légales', link: '/mentions-legales' },
@@ -24,6 +32,7 @@ const FooterDescription = () => (
       title="Voir le code source"
       href="https://github.com/betagouv/sante-psy"
       target="_blank"
+      rel="noopener noreferrer"
     >
       Voir le code source
     </Link>
@@ -37,10 +46,21 @@ const Footer = () => (
       <Logo href="https://www.enseignementsup-recherche.gouv.fr/">
         Ministère de l&lsquo;Enseignement Supérieur, de la Recherche et de l&lsquo;Innovation
       </Logo>
+      { footerBodyLinks.map(item => (
+        <FooterBodyItem key={footerBodyLinks.indexOf(item)}>
+          <Link
+            href={item.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {item.title}
+          </Link>
+        </FooterBodyItem>
+      ))}
     </FooterBody>
     <FooterBottom>
-      { footerLinks.map(item => (
-        <FooterLink key={footerLinks.indexOf(item)} href={item.link}>
+      { footerBottomLinks.map(item => (
+        <FooterLink key={footerBottomLinks.indexOf(item)} href={item.link}>
           {item.title}
         </FooterLink>
       ))}
