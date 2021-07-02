2c2c144ce593061e2bbda1f8c5b883ee4f87685a
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2021-07-02 18:01:26 +0200

    fix(footer): better external link & key management

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -10,18 +10,22 @@ import {
 } from '@dataesr/react-dsfr';
 
 const footerBodyLinks = [
-  { title: 'Contactez-nous', link: '/mentions-legales' },
-  { title: 'etudiant.gouv.fr', link: 'https://www.etudiant.gouv.fr/' },
-  { title: 'Enseignement supérieur', link: 'https://www.enseignementsup-recherche.gouv.fr/' },
-  { title: 'beta.gouv.fr', link: 'https://beta.gouv.fr/' },
+  { key: 'footer-body-link-1', title: 'Contactez-nous', link: '/mentions-legales', external: false },
+  { key: 'footer-body-link-2', title: 'etudiant.gouv.fr', link: 'https://www.etudiant.gouv.fr/', external: true },
+  { key: 'footer-body-link-3', title: 'Enseignement supérieur', link: 'https://www.enseignementsup-recherche.gouv.fr/', external: true },
+  { key: 'footer-body-link-4', title: 'beta.gouv.fr', link: 'https://beta.gouv.fr/', external: true },
 ];
 
 const footerBottomLinks = [
-  { title: 'Accessibilité: non conforme', link: '/mentions-legales#accessibilite' },
-  { title: 'Données personnelles et gestion des cookies', link: '/donnees-personnelles-et-gestion-des-cookies' },
-  { title: 'Mentions légales', link: '/mentions-legales' },
-  { title: 'En savoir plus', link: '/faq' },
-  { title: 'Nos statistiques', link: '/stats' },
+  { key: 'footer-bottom-link-1', title: 'Accessibilité: non conforme', link: '/mentions-legales#accessibilite' },
+  {
+    key: 'footer-bottom-link-2',
+    title: 'Données personnelles et gestion des cookies',
+    link: '/donnees-personnelles-et-gestion-des-cookies',
+  },
+  { key: 'footer-bottom-link-3', title: 'Mentions légales', link: '/mentions-legales' },
+  { key: 'footer-bottom-link-4', title: 'En savoir plus', link: '/faq' },
+  { key: 'footer-bottom-link-5', title: 'Nos statistiques', link: '/stats' },
 ];
 
 const FooterDescription = () => (
@@ -47,11 +51,11 @@ const Footer = () => (
         Ministère de l&lsquo;Enseignement Supérieur, de la Recherche et de l&lsquo;Innovation
       </Logo>
       { footerBodyLinks.map(item => (
-        <FooterBodyItem key={footerBodyLinks.indexOf(item)}>
+        <FooterBodyItem key={item.key}>
           <Link
             href={item.link}
-            target="_blank"
-            rel="noopener noreferrer"
+            target={item.external ? '_blank' : '_self'}
+            rel={item.external ? 'noopener noreferrer' : 'none'}
           >
             {item.title}
           </Link>
@@ -60,7 +64,7 @@ const Footer = () => (
     </FooterBody>
     <FooterBottom>
       { footerBottomLinks.map(item => (
-        <FooterLink key={footerBottomLinks.indexOf(item)} href={item.link}>
+        <FooterLink key={item.key} href={item.link}>
           {item.title}
         </FooterLink>
       ))}
