bb67c3b8668b51d2ae926a3cee22bb322fa62531
Xavierd <xavier.desoindre@hotmail.fr>
2021-07-15 12:12:55 +0200

    Remove warning

--- frontend/src/components/Psychologist/Profile/ConventionForm.jsx
+++ frontend/src/components/Psychologist/Profile/ConventionForm.jsx
@@ -55,10 +55,8 @@ const ConventionForm = ({ currentConvention, onConventionUpdated, checkDefaultVa
           : []}
       />
       <RadioGroup
-        value={convention.isConventionSigned}
         onChange={value => setConvention({ ...convention, isConventionSigned: value })}
         required
-        checked={convention.isConventionSigned}
         legend="Avez-vous déjà signé la convention ?"
         hint="Renseignez votre situation actuelle pour que nous puissions vous aider à avancer au besoin.
               Vous pourrez mettre à jour vos réponses plus tard si votre statut change."
@@ -66,12 +64,12 @@ const ConventionForm = ({ currentConvention, onConventionUpdated, checkDefaultVa
         <Radio
           data-test-id="signed-true"
           label="Oui"
-          value
+          value="true"
         />
         <Radio
           data-test-id="signed-false"
           label="Non"
-          value={false}
+          value="false"
         />
       </RadioGroup>
 
