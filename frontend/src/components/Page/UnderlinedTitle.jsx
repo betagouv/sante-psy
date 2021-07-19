346bfdd885e4bc6c3e70911d6d22a6186de5f327
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2021-07-19 16:22:37 +0200

    fix: underline title on multi line

--- frontend/src/components/Page/UnderlinedTitle.jsx
+++ frontend/src/components/Page/UnderlinedTitle.jsx
@@ -1,11 +1,9 @@
-import classNames from 'classnames';
 import React from 'react';
 
 import styles from './underlinedTitle.cssmodule.scss';
 
 const UnderlinedTitle = ({ title, big, className }) => (
-  <div className={classNames(styles.container, className)}>
-    <div className={big ? styles.bigUnderline : styles.underline} />
+  <div className={className}>
     <h1 className={big ? styles.bigTitle : styles.title}>{title}</h1>
   </div>
 );
