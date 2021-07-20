d97f4dfd3c57a0d64171c4c9041b96fa46b4c916
Damir <damir.sagadbekov@live.fr>
2021-07-20 16:13:14 +0200

    first steps

--- frontend/src/components/Footer/Mail.jsx
+++ frontend/src/components/Footer/Mail.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Mail = () => (
   <div className="fr-my-4w">
     <Link to="/contact">
-      Des questions ? Une difficulté ? Contactez nous !
+      Des questions ? Une difficulté ? Envoyez-nous un email à support-santepsyetudiant@beta.gouv.fr.
 
     </Link>
   </div>
