c921c6a98676cb024394b70f56954868dfd2bbc3
Sandrine Ricardo <68006779+sandrica89@users.noreply.github.com>
2021-08-03 19:30:51 +0200

    Refactor : move tests in dedicated directories (#365)
    * refactor: move tests in dedicated dir
    
    * fix: remove unused file
    
    * Useless eslint line
    
    Co-authored-by: Xavierd <xavier.desoindre@hotmail.fr>

---
+++ test/controllers/contactController.spec.ts
@@ -0,0 +1,122 @@
+import chai from 'chai';
+import app from '../../index';
+
+describe('contactController', () => {
+  describe('send', () => {
+    it('should send message', async () => chai.request(app)
+      .post('/api/contact')
+      .send({
+        name: 'Neris',
+        firstName: 'Iti',
+        email: 'iti@ner.is',
+        message: 'Pas content !',
+        reason: 'éligibilité',
+        user: 'étudiant',
+      })
+      .then(async (res) => {
+        res.status.should.equal(200);
+        res.body.message.should.equal('Votre message a bien été envoyé. Nous reviendrons vers vous rapidement.');
+      }));
+
+    const failValidation = async (payload, errorMessage) => chai.request(app)
+      .post('/api/contact')
+      .send(payload)
+      .then(async (res) => {
+        res.status.should.equal(400);
+        res.body.message.should.equal(errorMessage);
+      });
+
+    it('should not send message if name is missing', async () => {
+      await failValidation({
+        firstName: 'Iti',
+        email: 'iti@ner.is',
+        message: 'Pas content !',
+        reason: 'éligibilité',
+        user: 'étudiant',
+      }, 'Vous devez spécifier un nom.');
+    });
+
+    it('should not send message if firstname is missing', async () => {
+      await failValidation({
+        name: 'Neris',
+        email: 'iti@ner.is',
+        message: 'Pas content !',
+        reason: 'éligibilité',
+        user: 'étudiant',
+      }, 'Vous devez spécifier un prénom.');
+    });
+
+    it('should not send message if email is missing', async () => {
+      await failValidation({
+        name: 'Neris',
+        firstName: 'Iti',
+        message: 'Pas content !',
+        reason: 'éligibilité',
+        user: 'étudiant',
+      }, 'Vous devez spécifier un email valide.');
+    });
+
+    it('should not send message if email is invalid', async () => {
+      await failValidation({
+        name: 'Neris',
+        firstName: 'Iti',
+        email: 'nicetrydude',
+        message: 'Pas content !',
+        reason: 'éligibilité',
+        user: 'étudiant',
+      }, 'Vous devez spécifier un email valide.');
+    });
+
+    it('should not send message if reason is missing', async () => {
+      await failValidation({
+        name: 'Neris',
+        firstName: 'Iti',
+        email: 'iti@ner.is',
+        message: 'Pas content !',
+        user: 'étudiant',
+      }, 'Vous devez spécifier une raison.');
+    });
+
+    it('should not send message if reason is invalid', async () => {
+      await failValidation({
+        name: 'Neris',
+        firstName: 'Iti',
+        email: 'iti@ner.is',
+        message: 'Pas content !',
+        reason: 'pas content...',
+        user: 'étudiant',
+      }, 'Vous devez spécifier une raison.');
+    });
+
+    it('should not send message if user is missing', async () => {
+      await failValidation({
+        name: 'Neris',
+        firstName: 'Iti',
+        email: 'iti@ner.is',
+        message: 'Pas content !',
+        reason: 'éligibilité',
+      }, 'Vous devez préciser qui vous êtes.');
+    });
+
+    it('should not send message if user is invalid', async () => {
+      await failValidation({
+        name: 'Neris',
+        firstName: 'Iti',
+        email: 'iti@ner.is',
+        message: 'Pas content !',
+        reason: 'éligibilité',
+        user: 'me',
+      }, 'Vous devez préciser qui vous êtes.');
+    });
+
+    it('should not send message if message is missing', async () => {
+      await failValidation({
+        name: 'Neris',
+        firstName: 'Iti',
+        email: 'iti@ner.is',
+        reason: 'éligibilité',
+        user: 'étudiant',
+      }, 'Vous devez spécifier un message.');
+    });
+  });
+});
---
+++ test/utils/graphql.spec.ts
@@ -0,0 +1,19 @@
+import dotEnv from 'dotenv';
+import graphql from '../../utils/graphql';
+
+dotEnv.config();
+
+describe('GraphQL Handler', () => {
+  describe('getWhereConditionAfterCursor', () => {
+    it('should return the graphql condition with after as an argument', async () => {
+      const after = 'MzA2';
+      const result = `(after: "${after}")`;
+
+      graphql.getWhereConditionAfterCursor(after).should.equal(result);
+    });
+
+    it('should return an empty string if undefined', async () => {
+      graphql.getWhereConditionAfterCursor(undefined).should.equal('');
+    });
+  });
+});
