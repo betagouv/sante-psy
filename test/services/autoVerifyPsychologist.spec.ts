f03f21c4c75bc437de90636221cff42bb4e28cfc
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2021-08-09 10:45:46 +0200

    Add a timeout when waiting to run the tests (#367)

--- test/services/autoVerifyPsychologist.spec.ts
+++ test/services/autoVerifyPsychologist.spec.ts
@@ -1,31 +1,69 @@
 import sinon from 'sinon';
-import verifyPsychologist from '../../services/demarchesSimplifiees/verifyPsychologist';
 import autoVerifyPsychologists from '../../services/demarchesSimplifiees/autoVerify';
+import config from '../../utils/config';
+import graphql from '../../utils/sendGraphQLRequest';
 
 describe('autoVerifyPsychologist', () => {
-  let verifyPsychologistStub;
+  let executeMutationStub;
 
   beforeEach(() => {
-    verifyPsychologistStub = sinon.stub(verifyPsychologist, 'verifyPsychologist');
+    executeMutationStub = sinon.stub(graphql, 'executeMutation');
   });
 
   afterEach((done) => {
-    verifyPsychologistStub.restore();
+    executeMutationStub.restore();
     done();
   });
 
-  // FIXME!
-  it.skip('Should call verifyPsychologist twice with correct args', async () => {
+  it('Should call verifyPsychologist twice with correct args', async () => {
     await autoVerifyPsychologists();
 
-    sinon.assert.callCount(verifyPsychologistStub, 2);
+    sinon.assert.callCount(executeMutationStub, 4);
 
-    sinon.assert.calledWith(verifyPsychologistStub.getCall(0),
-      sinon.match(),
-      sinon.match());
+    const DOSSIER_ID_VALID = 'RG9zc2llci00ODkwOTEw';
+    const DOSSIER_ID_INVALID = 'RG9zc2llci00Nzg5MzM2';
+    const INSTRUCTOR_ID = config.demarchesSimplifieesInstructor;
 
-    sinon.assert.calledWith(verifyPsychologistStub.getCall(1),
-      sinon.match(),
-      sinon.match());
+    sinon.assert.calledWith(executeMutationStub.getCall(0),
+      sinon.match((query) => query.includes('mutation dossierModifierAnnotationText')),
+      sinon.match({
+        input: {
+          dossierId: DOSSIER_ID_INVALID,
+          instructeurId: INSTRUCTOR_ID,
+          annotationId: 'Q2hhbXAtMTY1NzQwNQ==',
+          value: sinon.match((value) => value.startsWith("Le dossier n'a pas passé la vérification automatique le ")),
+        },
+      }));
+
+    sinon.assert.calledWith(executeMutationStub.getCall(1),
+      sinon.match((query) => query.includes('mutation dossierModifierAnnotationText')),
+      sinon.match({
+        input: {
+          dossierId: DOSSIER_ID_VALID,
+          instructeurId: INSTRUCTOR_ID,
+          annotationId: 'Q2hhbXAtMTY1NzQwNQ==',
+          value: sinon.match((value) => value.startsWith('Dossier vérifié automatiquement le ')),
+        },
+      }));
+
+    sinon.assert.calledWith(executeMutationStub.getCall(2),
+      sinon.match((query) => query.includes('mutation dossierModifierAnnotationCheckbox')),
+      sinon.match({
+        input: {
+          dossierId: DOSSIER_ID_VALID,
+          instructeurId: INSTRUCTOR_ID,
+          annotationId: 'Q2hhbXAtMTY1NzQwMw==',
+          value: true,
+        },
+      }));
+
+    sinon.assert.calledWith(executeMutationStub.getCall(3),
+      sinon.match((query) => query.includes('mutation dossierPasserEnInstruction')),
+      sinon.match({
+        input: {
+          dossierId: DOSSIER_ID_VALID,
+          instructeurId: INSTRUCTOR_ID,
+        },
+      }));
   }).timeout(30000);
 });
