153cb2b9069156a26add9e4e12a79f22e0ba7e36
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2021-08-12 16:13:39 +0200

    feat: update script to insert coordinates to psy
    use api from gouv instead of nominatim
    extract api call to dedicated service
    process sequentially to delay calls because of limitations

--- scripts/insertCoordinatesToPsychologists.ts
+++ scripts/insertCoordinatesToPsychologists.ts
@@ -1,50 +1,39 @@
-import axios from 'axios/index';
+/* eslint-disable no-await-in-loop */
+
 import db from '../db/db';
 import { psychologistsTable } from '../db/tables';
-import { Psychologist } from '../types/Psychologist';
+import getAddrCoordinates from '../services/getAddrCoordinates';
 
-const sleep = (ms: number) : Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
+const delay = (ms = 1000) : Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
 
 const insertCoordinatesToPsychologists = async (): Promise<void> => {
   console.log('Adding location coordinates to all psychologists...');
 
   try {
-    let locationFound = 0;
     const allPsy = await db(psychologistsTable);
 
-    const promises = allPsy.map(async (psy : Psychologist) => {
-      // To be compliant with [Usage policy](https://operations.osmfoundation.org/policies/nominatim/)
-      await sleep(2000); // FIXME: this doesn't work to avoid multiple queries on same second!
+    for (let index = 0; index < allPsy.length; index++) {
+      const psy = allPsy[index];
 
-      // TODO: move coordinates retrieval on a dedicated service
-      const url = encodeURI(`https://nominatim.openstreetmap.org/search?q=${psy.address}&format=json&limit=1`);
-      console.debug('url', url);
+      if (psy.address) {
+        // To be compliant with limitation (50 requests per second)
+        // See https://geo.api.gouv.fr/faq)
+        await delay(25); // 1 request every 25ms => 40 requests per second
 
-      const response = await axios.get(url)
-        .catch((error) => {
-          console.log('error', error);
-          return Promise.reject(error);
-        });
+        const coord = await getAddrCoordinates(psy.address);
 
-      // FIXME: response.data always empty even if the browser displays results (am I banned?)
-      if (response.data.length > 0) {
-        locationFound++;
-        console.debug('Update', psy.dossierNumber, 'with', response.data[0].lon, response.data[0].lat);
-        return db(psychologistsTable)
-          .where({ dossierNumber: psy.dossierNumber })
-          .update({
-            longitude: response.data[0].lon,
-            latitude: response.data[0].lat,
-          });
+        if (coord) {
+          await db(psychologistsTable)
+            .where({ dossierNumber: psy.dossierNumber })
+            .update({
+              longitude: coord.longitude,
+              latitude: coord.latitude,
+            });
+        }
       }
+    }
 
-      console.debug('Not found');
-      return Promise.resolve(0);
-    });
-
-    await Promise.all(promises);
-
-    console.log('DONE:', locationFound, 'psy locations found over', allPsy.length);
+    console.log('Done');
     process.exit(0);
   } catch (err) {
     console.error(err);
---
+++ services/getAddrCoordinates.ts
@@ -0,0 +1,35 @@
+import axios from 'axios/index';
+import { Coordinates } from '../types/Coordinates';
+
+const getAddrCoordinates = async (address: string): Promise<Coordinates|void> => {
+  const MIN_SCORE = 0.55;
+
+  const url = encodeURI(`https://api-adresse.data.gouv.fr/search/?q=${address}&limit=1`);
+  const response = await axios.get(url)
+    .catch((error) => {
+      console.log('error', error);
+    });
+
+  if (response && response.data.features && response.data.features.length > 0) {
+    const feature = response.data.features[0];
+    const longitude = feature.geometry.coordinates[0];
+    const latitude = feature.geometry.coordinates[1];
+    const { score } = feature.properties;
+    const { label } = feature.properties;
+    console.debug(`"${address}" ; "${label}" ; "${score}"`);
+
+    if (score > MIN_SCORE) {
+      return Promise.resolve({
+        longitude,
+        latitude,
+      });
+    }
+    // Insufficient score
+    return Promise.resolve();
+  }
+  // Not found
+  console.debug(`"${address}" ; "" ; "0"`);
+  return Promise.resolve();
+};
+
+export default getAddrCoordinates;
