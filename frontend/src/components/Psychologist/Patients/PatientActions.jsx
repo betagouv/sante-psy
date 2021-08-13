bb59cce29e72e7ce2a396cb6525dafb6d4f52574
Xavierd <xavier.desoindre@hotmail.fr>
2021-08-13 14:58:12 +0200

    Add the seance button in the patient table

--- frontend/src/components/Psychologist/Patients/PatientActions.jsx
+++ frontend/src/components/Psychologist/Patients/PatientActions.jsx
@@ -1,46 +1,59 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { Button } from '@dataesr/react-dsfr';
+import classnames from 'classnames';
+
+const ShrinkableButton = props => (
+  <>
+    <Button
+      {...props}
+      data-test-id={props['data-test-id'] ? `${props['data-test-id']}-small` : undefined}
+      className={classnames(props.className, 'fr-displayed-xs fr-hidden-md')}
+    />
+    <Button
+      {...props}
+      data-test-id={props['data-test-id'] ? `${props['data-test-id']}-large` : undefined}
+      className={classnames(props.className, 'fr-btn--icon-left fr-hidden-xs fr-displayed-md')}
+    >
+      {props.children}
+    </Button>
+  </>
+);
 
 const PatientActions = ({ patient, deletePatient }) => {
   const history = useHistory();
   return (
     <div className="list-actions">
-      <Button
-        data-test-id="delete-etudiant-button-small"
+      <ShrinkableButton
+        data-test-id="appointment-etudiant-button"
         onClick={deletePatient}
         secondary
         size="sm"
-        className="fr-fi-delete-line fr-displayed-xs fr-hidden-md fr-float-right"
-        aria-label="Supprimer"
-      />
-      <Button
-        data-test-id="delete-etudiant-button-large"
-        onClick={deletePatient}
-        secondary
-        size="sm"
-        className="fr-fi-delete-line fr-btn--icon-left fr-hidden-xs fr-displayed-md fr-float-right"
+        className="fr-fi-calendar-line"
+        aria-label="seance"
       >
-        Supprimer
-      </Button>
-
-      <Button
-        data-test-id="update-etudiant-button-small"
+        Séance
+      </ShrinkableButton>
+      <ShrinkableButton
+        data-test-id="update-etudiant-button"
         onClick={() => history.push(`/psychologue/modifier-etudiant/${patient.id}`)}
         secondary
         size="sm"
-        className="fr-fi-edit-line fr-displayed-xs fr-hidden-md fr-float-right"
+        className="fr-fi-edit-line"
         aria-label={!patient.hasFolderCompleted ? 'Compléter' : 'Modifier'}
-      />
-      <Button
-        data-test-id="update-etudiant-button-large"
-        onClick={() => history.push(`/psychologue/modifier-etudiant/${patient.id}`)}
-        secondary
-        size="sm"
-        className="fr-fi-edit-line fr-btn--icon-left fr-hidden-xs fr-displayed-md fr-float-right"
       >
         { !patient.hasFolderCompleted ? 'Compléter' : 'Modifier'}
-      </Button>
+      </ShrinkableButton>
+      <ShrinkableButton
+        data-test-id="delete-etudiant-button"
+        onClick={deletePatient}
+        secondary
+        size="sm"
+        className="fr-fi-delete-line"
+        aria-label="Supprimer"
+      >
+        Supprimer
+      </ShrinkableButton>
     </div>
   );
 };
