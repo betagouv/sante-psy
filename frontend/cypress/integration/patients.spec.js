f2626f0c75abd75cb9bc6bbfc8cd2ce155d91261
Xavierd <xavier.desoindre@hotmail.fr>
2021-08-24 16:01:25 +0200

    Add number of appointments per patient

--- frontend/cypress/integration/patients.spec.js
+++ frontend/cypress/integration/patients.spec.js
@@ -27,16 +27,22 @@ describe('Patient', () => {
       cy.get('[data-test-id="etudiant-table"] tr')
         .should('have.length', 6);
       cy.get('[data-test-id="etudiant-row-missing-info"]')
-        .should('have.length', 1);
+        .should('have.length', 2);
       cy.get('[data-test-id="etudiant-row-complete-info"]')
-        .should('have.length', 4);
+        .should('have.length', 3);
+      cy.get('[data-test-id="etudiant-table"] td')
+        .eq(1)
+        .should(
+          'have.text',
+          'Informations manquantes : nom du docteur, établissement scolaire, adresse du docteur, date de naissance, statut étudiant, orientation médicale',
+        );
     });
   });
 
   describe('Update', () => {
     it('should update existing etudiant and update completion info', () => {
       cy.get('[data-test-id="update-etudiant-button-large"]')
-        .first()
+        .eq(1)
         .click();
       cy.wait('@etudiant');
 
@@ -53,8 +59,10 @@ describe('Patient', () => {
 
       cy.get('[data-test-id="etudiant-table"] tr')
         .should('have.length', 6);
+      cy.get('[data-test-id="etudiant-row-missing-info"]')
+        .should('have.length', 1);
       cy.get('[data-test-id="etudiant-row-complete-info"]')
-        .should('have.length', 5);
+        .should('have.length', 4);
 
       cy.get('[data-test-id="notification-success"] p')
         .should(
@@ -68,20 +76,6 @@ describe('Patient', () => {
     it('should remove etudiant with incomplete info and notify user', () => {
       cy.get('[data-test-id="delete-etudiant-button-large"]')
         .first()
-        .click();
-      cy.wait('@deleteEtudiants');
-      cy.get('[data-test-id="etudiant-table"] tr')
-        .should('have.length', 5);
-      cy.get('[data-test-id="notification-success"] p')
-        .should(
-          'have.text',
-          "L'étudiant a bien été supprimé.",
-        );
-    });
-
-    it('should remove etudiant with complete info and notify user', () => {
-      cy.get('[data-test-id="delete-etudiant-button-large"]')
-        .last()
         .click();
       cy.wait('@deleteEtudiants');
       cy.get('[data-test-id="etudiant-table"] tr')
