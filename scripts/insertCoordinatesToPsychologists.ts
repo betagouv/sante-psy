45666abb1538489a38f36e91f0742d86665a1b75
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2021-09-06 18:48:15 +0200

    fix: update condition on insert coordinates script

--- scripts/insertCoordinatesToPsychologists.ts
+++ scripts/insertCoordinatesToPsychologists.ts
@@ -22,7 +22,7 @@ const insertCoordinatesToPsychologists = async (): Promise<void> => {
 
         const coord = await getAddressCoordinates(psy.address);
 
-        if (coord) {
+        if (coord && coord.longitude) {
           await db(psychologistsTable)
             .where({ dossierNumber: psy.dossierNumber })
             .update({
