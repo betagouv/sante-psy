998d556358c59de55d1473f77c272bb668dc8416
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2021-09-07 14:28:52 +0200

    Improve FAQ content (#408)
    * Improve FAQ content
    
    * feat: use unbreakable spaces
    
    * fix: use unicode for unbreakable spaces
    
    * feat: use unicode unbreakable spaces everywhere!
    
    Co-authored-by: Sandrine Ricardo <sandrine.ricardo@gmail.com>

--- frontend/src/services/faq/psychologue/process.js
+++ frontend/src/services/faq/psychologue/process.js
@@ -2,36 +2,36 @@
 /* eslint-disable max-len */
 module.exports = config => [
   {
-    question: 'Je suis déjà des étudiants. Puis-je continuer à les suivre dans le cadre du dispositif Santé Psy Étudiant ?',
+    question: 'Je suis déjà des étudiants. Puis-je continuer à les suivre dans le cadre du dispositif Santé Psy Étudiant\u00A0?',
     answer: `Si les étudiants que vous prenez en charge actuellement souhaitent bénéficier du dispositif et \
     poursuivre leur consultation avec vous, il est nécessaire qu’ils soient orientés vers un psychologue par le SSU dont \
     ils dépendent ou un médecin généraliste. Parallèlement, pour pouvoir continuer à suivre ces étudiants, \
-    vous devez être agréé pour participer au dispositif : renseignez le formulaire en ligne <a href="${config.demarchesSimplifieesUrl}" target="_blank" rel="noopener noreferrer">(cliquez ici pour accéder au formulaire)</a>. \
+    vous devez être agréé pour participer au dispositif\u00A0: renseignez le formulaire en ligne <a href="${config.demarchesSimplifieesUrl}" target="_blank" rel="noopener noreferrer">(cliquez ici pour accéder au formulaire)</a>. \
     Si vous remplissez les conditions de participation et acceptez les modalités de prise en charge des étudiants (tarif, lien avec le corps médical notamment), \
     une convention avec l’université la plus proche de votre lieu d’exercice vous sera proposée et vous serez inscrit sur la liste des psychologues agréés par le SSU \
     mise à disposition des étudiants et médecins généralistes.`,
   },
   {
-    question: 'Les étudiants peuvent-ils directement venir voir le psychologue ?',
+    question: 'Les étudiants peuvent-ils directement venir voir le psychologue\u00A0?',
     answer: 'Dans le cadre du dispositif Santé Psy Étudiant, les étudiants doivent consulter le service de santé universitaire ou \
     un médecin généraliste qui peut les orienter vers un psychologue partenaire de la démarche.',
   },
   {
-    question: 'Comment un étudiant sait s’il a un SSU ? Comment fait-il s’il n’en a pas ?',
+    question: 'Comment un étudiant sait s’il a un SSU\u00A0? Comment fait-il s’il n’en a pas\u00A0?',
     answer: 'Les étudiants peuvent savoir si leur établissement dispose d’un service de santé universitaire (SSU) ou s’ils ont accès au SSU \
-    d’un autre établissement (par convention) en consultant le site de leur université / école ou en leur téléphonant. \
+    d’un autre établissement (par convention) en consultant le site de leur université\u00A0/ école ou en leur téléphonant. \
     Si l’étudiant n’a pas accès à un SSU, il peut se rendre chez un médecin généraliste qui l’orientera en cas de besoin vers un psychologue partenaire du dispositif.\
     D’une façon générale, les établissements disposant d’un SSU sont invités à ouvrir autant que possible leur service aux écoles partenaires ou partageant le site.',
   },
   {
-    question: 'Qui doit renouveler les séances ? Faut-il nécessairement repasser chez le médecin ?',
+    question: 'Qui doit renouveler les séances\u00A0? Faut-il nécessairement repasser chez le médecin\u00A0?',
     answer: 'Au-delà d’un premier cycle de trois séances, l’étudiant peut être de nouveau orienté, \
     obligatoirement par le médecin du service de santé universitaire ou le médecin généraliste, \
     vers un psychologue pour un second cycle de trois séances au plus. \
     Le passage par le médecin répond à la volonté d’inscrire l’étudiant dans un parcours de soin rigoureux.',
   },
   {
-    question: 'Si l’étudiant le souhaite, au-delà de la première consultation "d’orientation", peut-on lui proposer de continuer à le suivre pour les trois séances remboursées ?',
+    question: 'Si l’étudiant le souhaite, au-delà de la première consultation "d’orientation", peut-on lui proposer de continuer à le suivre pour les trois séances remboursées\u00A0?',
     answer: 'L’étudiant qui a accès à la liste des psychologues participant au dispositif, \
     choisit celui qu’il souhaite consulter et prend rendez-vous avec lui. Il revient au psychologue de déterminer, lors de la première consultation, \
     si l’étudiant a besoin d’une, deux ou trois séances avec lui. Dans l’hypothèse où à l’issue des trois premières séances, \
@@ -40,13 +40,13 @@ module.exports = config => [
   },
   {
     question: 'Il est indiqué que les psychologues feront acte de candidature via un formulaire simplifié en ligne et \
-    qu’une plateforme numérique permettra de déclarer le nombre de consultations effectuées. Serait-il possible d’y avoir accès ou bien est-ce réservé aux SSU ?',
+    qu’une plateforme numérique permettra de déclarer le nombre de consultations effectuées. Serait-il possible d’y avoir accès ou bien est-ce réservé aux SSU\u00A0?',
     answer: 'C’est au psychologue de faire la déclaration des séances faites une fois qu’il est inscrit via le formulaire en ligne et \
     que son dossier est accepté.  Cette déclaration se fait sur la plateforme Santé Psy Étudiant, dans l’espace personnel du psychologue une fois le dossier accepté.',
   },
   {
     question: "Je reçois beaucoup de demande d'étudiants, je souhaite ne plus être affiché dans la liste publique des psychologues partenaires, \
-    tout en restant dans le dispositif, est-ce possible ?",
+    tout en restant dans le dispositif, est-ce possible\u00A0?",
     answer: 'Vous avez la possibilité de vous inscrire sur la plateforme et ainsi faire bénéficier les étudiants de séances dans le cadre du dispositif. \
     Dans le cas d’une forte demande des étudiants, vous avez la possibilité de refuser par manque de disponibilités lorsqu’un étudiant vous contacte ou éventuellement de l’orienter vers un autre psychologue du dispositif.',
   },
--- frontend/src/services/faq/psychologue/reimbursement.js
+++ frontend/src/services/faq/psychologue/reimbursement.js
@@ -3,21 +3,21 @@
 module.exports = () => [
   {
     question: 'Comment a été fixé le tarif de 30 euros pour une consultation, sachant que les tarifs varient selon les régions, \
-    selon les formations complémentaires et spécialités de chaque professionnel ?',
-    answer: 'S’agissant d’un dispositif national, le tarif doit être identique sur l’ensemble du territoire et il a été fixé à 30 € par le ministère des solidarités et de la santé.',
+    selon les formations complémentaires et spécialités de chaque professionnel\u00A0?',
+    answer: 'S’agissant d’un dispositif national, le tarif doit être identique sur l’ensemble du territoire et il a été fixé à 30\u00A0€ par le ministère des solidarités et de la santé.',
   },
   {
     question: 'Est-il prévu soit un allégement des taxes pour que les psychologues \
-    investis dans ce dispositif ne travaillent pas bénévolement ? Est-il simplement prévu que des professionnels donnent leur temps ?',
+    investis dans ce dispositif ne travaillent pas bénévolement\u00A0? Est-il simplement prévu que des professionnels donnent leur temps\u00A0?',
     answer: 'À ce stade, il n’est prévu aucune mesure particulière.',
   },
   {
-    question: 'Pourquoi les consultations sont-elles fixées à 45 minutes ?',
+    question: 'Pourquoi les consultations sont-elles fixées à 45 minutes\u00A0?',
     answer: 'La durée des séances ainsi que, par ailleurs, les critères relatifs au diplôme ou à \
     l’expérience ont été établis avec le concours de la fédération française des psychologues et de psychologie.',
   },
   {
-    question: 'Comment se passe le remboursement ? Quel délai ?',
+    question: 'Comment se passe le remboursement\u00A0? Quel délai\u00A0?',
     answer: 'Les psychologues partenaires du dispositif sont agréés par les services de \
     santé universitaire (SSU) et signent une convention avec l’établissement dont dépend le SSU qui les a agréés. \
     Cette convention précise les modalités de paiement des séances réalisées dans le cadre du dispositif. \
@@ -27,7 +27,11 @@ module.exports = () => [
     Le paiement intervient dans le délai global de paiement, soit 30 jours à partir du moment où les pièces justificatives du paiement sont réunies.',
   },
   {
-    question: 'Quelle est la procédure à suivre pour se faire rembourser ?',
-    answer: 'La première étape est de déclarer vos séances réalisées sur votre espace psychologue. Une fois la convention signée avec l’université, vous devez ensuite lui adresser vos facturations selon ses modalités d’envoi. <br /> Si votre convention est en cours, nous vous invitons à patienter. L’université de rattachement prendra contact avec vous sous 2 à 4 semaines pour signer votre convention et vous transmettre ses modalités de facturation (numéro de bon de commande si nécessaire, mode d’envoi etc.).',
+    question: 'Quelle est la procédure à suivre pour se faire rembourser\u00A0?',
+    answer: 'La première étape est de déclarer vos séances réalisées sur votre espace psychologue. \
+    Une fois la convention signée avec l’université, vous devez ensuite lui adresser vos facturations \
+    selon ses modalités d’envoi. <br /> Si votre convention est en cours, nous vous invitons à patienter. \
+    L’université de rattachement prendra contact avec vous sous 2 à 4 semaines pour signer votre convention \
+    et vous transmettre ses modalités de facturation (numéro de bon de commande si nécessaire, mode d’envoi etc.).',
   },
 ];
