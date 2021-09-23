f18d1dda4714f8d5d7dad1c0b338b9d694fe4a42
Sandrine Ricardo <68006779+sandrica89@users.noreply.github.com>
2021-09-23 15:59:27 +0200

    Update FAQ (retractation)

--- frontend/src/services/faq/psychologue/retractation.js
+++ frontend/src/services/faq/psychologue/retractation.js
@@ -1,10 +1,8 @@
 /* eslint-disable no-multi-str */
-/* eslint-disable max-len */
-module.exports = config => [
+module.exports = () => [
   {
     question: 'Je souhaite être retiré du dispositif, comment faire\u00A0?',
-    answer: `Pour être retiré du dispositif Santé Psy Étudiant, envoyez-nous un email \
-    à <a href="mailto:${config.contactEmail}">${config.contactEmail}</a> \
-    En nous indiquant votre volonté d'être retiré du dispositif et en précisant si vous avez déjà effectué des séances.`,
+    answer: 'Pour être retiré du dispositif Santé Psy Étudiant, rendez-vous dans votre espace psychologue \
+    sur la page "Mes Informations" et cliquez sur "Retirez mes informations de l’annuaire" en bas de la page.',
   },
 ];
