39469fe43a8afe416065d48e11a6c9559b72fe90
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2021-10-07 10:45:01 +0200

    fix: hide form after validation

--- frontend/src/components/Psychologist/Profile/InactiveProfile.jsx
+++ frontend/src/components/Psychologist/Profile/InactiveProfile.jsx
@@ -20,112 +20,117 @@ const InactiveProfile = () => {
     }
   };
   const canValidate = reason
-  && (reason !== 'other' || otherReason.trim() !== '');
+    && (reason !== 'other' || otherReason.trim() !== '');
 
   return (
     <Page
       background="blue"
     >
-      {done && (
+      {done ? (
         <Alert
           className="fr-mb-2w"
           type="success"
           {...done}
         />
+      ) : (
+        <>
+          <RadioGroup
+            legend="Vous nous avez indiqué vouloir quitter le dispositif Santé Psy Etudiant.
+            Afin d’améliorer ce dernier, pouvez-vous nous indiquer la raison de ce choix :"
+            ariaLabel="raison"
+            name="reason"
+            value={reason}
+            onChange={selectReason}
+          >
+            <Radio
+              data-test-id="radio-reason-toomuch"
+              label="Je ne reçois pas assez de demandes"
+              value="toofew"
+            />
+            <Radio
+              data-test-id="radio-reason-toomuch"
+              label="Je reçois trop de demandes"
+              value="toomuch"
+            />
+            <Radio
+              data-test-id="radio-reason-toomuch"
+              label="Je n'arrive pas à me connecter"
+              value="connection"
+            />
+            <Radio
+              data-test-id="radio-reason-toomuch"
+              label="Je ne suis plus en accord avec le dispositif"
+              value="disagree"
+            />
+            <Radio
+              data-test-id="radio-reason-reimbursments"
+              label="Je suis en attente de mes remboursements"
+              value="reimbursments"
+            />
+            <Radio
+              data-test-id="radio-reason-convention"
+              label="Je suis en attente de ma convention"
+              value="convention"
+            />
+            <Radio
+              data-test-id="radio-reason-other"
+              label="Autre"
+              onChange={e => {
+                setReason('other');
+                setDisplayReason(e.target.checked);
+              }}
+              value="other"
+            />
+            {displayReason && (
+              <TextInput
+                data-test-id="radio-reason-other-input"
+                required
+                value={otherReason}
+                onChange={e => setOtherReason(e.target.value)}
+                textarea
+              />
+            )}
+          </RadioGroup>
+          <ButtonGroup isInlineFrom="xs">
+            <Button
+              data-test-id="suspend-button"
+              icon="fr-fi-eye-off-line"
+              onClick={() => {
+                agent.Psychologist.inactive(getReason(), token)
+                  .then(() => {
+                    setDone({
+                      title: "Vos informations ne sont plus visibles sur l'annuaire",
+                      description: 'Vous pouvez redevenir visible à tout moment sur votre espace psychologue',
+                    });
+                    window.scrollTo(0, 0);
+                  })
+                  .catch(() => window.scrollTo(0, 0));
+              }}
+              disabled={!canValidate}
+            >
+              Quitter le dispositif
+            </Button>
+            <Button
+              icon="fr-fi-eye-line"
+              secondary
+              onClick={() => {
+                agent.Psychologist.active(token)
+                  .then(() => {
+                    setDone({
+                      title: 'Merci',
+                      description:
+                        'Nous avons bien pris en compte votre réponse et vous remercions pour votre investissement !',
+                    });
+                    window.scrollTo(0, 0);
+                  })
+                  .catch(() => window.scrollTo(0, 0));
+              }}
+            >
+              Non, je veux rester
+            </Button>
+          </ButtonGroup>
+        </>
       )}
-      <RadioGroup
-        legend="Vous nous avez indiqué vouloir quitter le dispositif Santé Psy Etudiant. Afin d’améliorer ce dernier, pouvez-vous nous indiquer la raison de ce choix :"
-        ariaLabel="raison"
-        name="reason"
-        value={reason}
-        onChange={selectReason}
-      >
-        <Radio
-          data-test-id="radio-reason-toomuch"
-          label="Je ne reçois pas assez de demandes"
-          value="toofew"
-        />
-        <Radio
-          data-test-id="radio-reason-toomuch"
-          label="Je reçois trop de demandes"
-          value="toomuch"
-        />
-        <Radio
-          data-test-id="radio-reason-toomuch"
-          label="Je n'arrive pas à me connecter"
-          value="connection"
-        />
-        <Radio
-          data-test-id="radio-reason-toomuch"
-          label="Je ne suis plus en accord avec le dispositif"
-          value="disagree"
-        />
-        <Radio
-          data-test-id="radio-reason-reimbursments"
-          label="Je suis en attente de mes remboursements"
-          value="reimbursments"
-        />
-        <Radio
-          data-test-id="radio-reason-convention"
-          label="Je suis en attente de ma convention"
-          value="convention"
-        />
-        <Radio
-          data-test-id="radio-reason-other"
-          label="Autre"
-          onChange={e => {
-            setReason('other');
-            setDisplayReason(e.target.checked);
-          }}
-          value="other"
-        />
-        {displayReason && (
-          <TextInput
-            data-test-id="radio-reason-other-input"
-            required
-            value={otherReason}
-            onChange={e => setOtherReason(e.target.value)}
-            textarea
-          />
-        )}
-      </RadioGroup>
-      <ButtonGroup isInlineFrom="xs">
-        <Button
-          data-test-id="suspend-button"
-          icon="fr-fi-eye-off-line"
-          onClick={() => {
-            agent.Psychologist.inactive(getReason(), token)
-              .then(() => {
-                setDone({
-                  title: "Vos informations ne sont plus visibles sur l'annuaire",
-                  description: 'Vous pouvez redevenir visible à tout moment sur votre espace psychologue',
-                });
-                window.scrollTo(0, 0);
-              })
-              .catch(() => window.scrollTo(0, 0));
-          }}
-          disabled={!canValidate}
-        >
-          Quitter le dispositif
-        </Button>
-        <Button
-          icon="fr-fi-eye-line"
-          secondary
-          onClick={() => {
-            agent.Psychologist.active(token)
-              .then(() => {
-                setDone({
-                  title: 'Merci',
-                  description: 'Nous avons bien pris en compte votre réponse et vous remercions pour votre investissement !',
-                });
-                window.scrollTo(0, 0);
-              })
-              .catch(() => window.scrollTo(0, 0));
-          }}
-        >
-          Non, je veux rester
-        </Button>
-      </ButtonGroup>
     </Page>
   );
 };
