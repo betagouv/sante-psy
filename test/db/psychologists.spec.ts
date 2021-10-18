cedb2082ebc3ea32f6f12514c67a205d832e6608
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2021-10-18 21:43:22 +0200

    test: remove useless test

--- test/db/psychologists.spec.ts
+++ test/db/psychologists.spec.ts
@@ -80,31 +80,6 @@ describe('DB Psychologists', () => {
       assert.isFalse(updatedPsySPE.selfModified);
     });
 
-    it('should not update useFirstNames', async () => {
-      const psyDS = create.getOnePsy();
-
-      // First save psy from DS
-      await dbPsychologists.upsertMany([psyDS]);
-      const psySPE = await dbPsychologists.getById(psyDS.dossierNumber);
-      assert.isNull(psySPE.useFirstNames);
-      assert.isNull(psySPE.useLastName);
-
-      // Update from DB -> useFirstNames
-      await db(psychologistsTable)
-        .where('dossierNumber', psyDS.dossierNumber)
-        .update({ useFirstNames: 'My Little Name' });
-
-      // Update from DS -> new firstNames
-      const newPsyDS = { ...psyDS };
-      newPsyDS.firstNames = 'New firstname';
-      await dbPsychologists.upsertMany([newPsyDS]);
-
-      // Assert that firstNames changed but not useFirstNames
-      const updatedPsySPE = await dbPsychologists.getById(psyDS.dossierNumber);
-      updatedPsySPE.firstNames.should.be.equal('New firstname');
-      updatedPsySPE.useFirstNames.should.be.equal('My Little Name');
-    });
-
     it('should not update region if self modified', async () => {
       const psyDS = create.getOnePsy();
 
