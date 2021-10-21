192dba48bc7ddd8e7bdf04acd38335071b377a4e
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2021-10-21 09:01:04 +0200

    fix: add env var to handle all mail services
    - maildev: secure false, requireTLS false
    - mailjet: secure true, requireTLS true
    - tipimail: secure false, requireTLS true (default)

--- utils/email.ts
+++ utils/email.ts
@@ -6,9 +6,9 @@ const mailTransport = nodemailer.createTransport({
   debug: config.mail.debug,
   host: config.mail.host,
   port: config.mail.port,
-  // ignoreTLS: false,
-  requireTLS: true,
-  secure: false,
+  ignoreTLS: !config.mail.requireTLS,
+  requireTLS: config.mail.requireTLS,
+  secure: config.mail.secure,
   auth: {
     user: config.mail.auth.user,
     pass: config.mail.auth.pass,
