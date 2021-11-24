a5a41c145a6e75ef95b3dc137a637bc653428a42
Sandrine Ricardo <68006779+sandrica89@users.noreply.github.com>
2021-11-24 18:35:45 +0100

    Add axeptio sdk url in csp config (#457)
    * fix: add axeptio sdk url in csp config
    
    * chore: dummy change to trigger deploy
    
    * fix: add axeptio client url in csp config

--- utils/csp-config.ts
+++ utils/csp-config.ts
@@ -8,6 +8,7 @@ export default helmet.contentSecurityPolicy({
       'https://stats.data.gouv.fr/',
       // Matomo script hash
       "'sha256-4sJsjD6jok0r8RIemFeNZ1nEQfYv6qdzYIaUCTrSIhs='",
+      'https://static.axept.io/sdk-slim.js',
       'https://connect.facebook.net',
       'https://www.googletagmanager.com/',
     ],
@@ -15,7 +16,6 @@ export default helmet.contentSecurityPolicy({
       "'self'",
       'https://stats.data.gouv.fr/',
       'https://*.tile.openstreetmap.org/',
-      'https://www.facebook.com/',
       'data:',
     ],
     'frame-src': ['https://stats.santepsyetudiant.beta.gouv.fr'],
@@ -23,6 +23,7 @@ export default helmet.contentSecurityPolicy({
       "'self'",
       'https://nominatim.openstreetmap.org',
       'https://stats.data.gouv.fr/',
+      'https://client.axept.io/',
     ],
   },
 });
