f9eb2b48a7b2490d9acbd34f5ac2533ec118bc9a
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2021-11-25 16:56:44 +0100

    fix: remove script balise usage

--- frontend/src/services/trackAds.js
+++ frontend/src/services/trackAds.js
@@ -1,22 +1,55 @@
+/* eslint-disable func-names */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable prefer-rest-params */
+/* eslint-disable prefer-spread */
+/* eslint-disable no-multi-assign */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-expressions */
 const initAxeptio = () => {
-  addScript('/scripts/axeptio.js');
+  window.axeptioSettings = {
+    clientId: '61967f3fde10ba10101a4ca3',
+    cookiesVersion: 'santé psy étudiants-base',
+  };
+
+  (function (d, s) {
+    const t = d.getElementsByTagName(s)[0]; const
+      e = d.createElement(s);
+    e.async = true; e.src = '//static.axept.io/sdk-slim.js';
+    t.parentNode.insertBefore(e, t);
+  }(document, 'script'));
 };
 
 const initGoogleAds = () => {
-  addScript('/scripts/googleAds.js');
-};
-
-const initFacebookAds = () => {
-  addScript('/scripts/facebookAds.js');
-};
-
-const addScript = src => {
   const el = document.createElement('script');
-  el.setAttribute('src', src);
+  el.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=AW-10803675495');
   el.setAttribute('async', true);
   if (document.body !== null) {
     document.body.appendChild(el);
   }
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { window.dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', 'AW-10803675495');
+};
+
+const initFacebookAds = () => {
+  !(function (f, b, e, v, n, t, s) {
+    if (f.fbq) return; n = f.fbq = function () {
+      n.callMethod
+        ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+    n.queue = []; t = b.createElement(e); t.async = !0;
+    t.src = v; s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+  }(
+    window,
+    document,
+    'script',
+    'https://connect.facebook.net/en_US/fbevents.js',
+  ));
+  window.fbq('init', '3078857952370640');
 };
 
 const trackGoogleAds = () => {
