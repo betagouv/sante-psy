a6e89364b970a75ca80030e129f92e0962534750
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2021-12-24 09:56:47 +0100

    Fix DS test (#477)

--- test/services/autoVerifyPsychologist.spec.ts
+++ test/services/autoVerifyPsychologist.spec.ts
@@ -21,24 +21,11 @@ describe('autoVerifyPsychologist', () => {
     sinon.assert.callCount(executeMutationStub, 4);
 
     const DOSSIER_ID_VALID = 'RG9zc2llci00ODkwOTEw';
-    const DOSSIER_ID_INVALID = 'RG9zc2llci00Nzg5MzM2';
+    const DOSSIER_ID_INVALID = 'RG9zc2llci03MTk1MDYx';
     const INSTRUCTOR_ID = config.demarchesSimplifiees.instructor;
 
     sinon.assert.calledWith(
       executeMutationStub.getCall(0),
-      sinon.match((query) => query.includes('mutation dossierModifierAnnotationText')),
-      sinon.match({
-        input: {
-          dossierId: DOSSIER_ID_INVALID,
-          instructeurId: INSTRUCTOR_ID,
-          annotationId: 'Q2hhbXAtMTY1NzQwNQ==',
-          value: sinon.match((value) => value.startsWith("Le dossier n'a pas passé la vérification automatique le ")),
-        },
-      }),
-    );
-
-    sinon.assert.calledWith(
-      executeMutationStub.getCall(1),
       sinon.match((query) => query.includes('mutation dossierModifierAnnotationText')),
       sinon.match({
         input: {
@@ -51,7 +38,7 @@ describe('autoVerifyPsychologist', () => {
     );
 
     sinon.assert.calledWith(
-      executeMutationStub.getCall(2),
+      executeMutationStub.getCall(1),
       sinon.match((query) => query.includes('mutation dossierModifierAnnotationCheckbox')),
       sinon.match({
         input: {
@@ -64,12 +51,25 @@ describe('autoVerifyPsychologist', () => {
     );
 
     sinon.assert.calledWith(
-      executeMutationStub.getCall(3),
+      executeMutationStub.getCall(2),
       sinon.match((query) => query.includes('mutation dossierPasserEnInstruction')),
       sinon.match({
         input: {
           dossierId: DOSSIER_ID_VALID,
           instructeurId: INSTRUCTOR_ID,
+        },
+      }),
+    );
+
+    sinon.assert.calledWith(
+      executeMutationStub.getCall(3),
+      sinon.match((query) => query.includes('mutation dossierModifierAnnotationText')),
+      sinon.match({
+        input: {
+          dossierId: DOSSIER_ID_INVALID,
+          instructeurId: INSTRUCTOR_ID,
+          annotationId: 'Q2hhbXAtMTY1NzQwNQ==',
+          value: sinon.match((value) => value.startsWith("Le dossier n'a pas passé la vérification automatique le ")),
         },
       }),
     );
