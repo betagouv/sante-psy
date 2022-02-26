f47374509ec14e87b8a90b00b768c0e02cab0474
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2022-02-26 15:54:55 +0100

    test: student db

---
+++ test/db/students.spec.ts
@@ -0,0 +1,103 @@
+import { expect } from 'chai';
+import dotEnv from 'dotenv';
+import faker from 'faker';
+import { v4 as uuidv4 } from 'uuid';
+import db from '../../db/db';
+import dbStudents from '../../db/students';
+import { studentsTable } from '../../db/tables';
+import { Student } from '../../types/Student';
+import clean from '../helper/clean';
+
+dotEnv.config();
+
+describe('DB Students', () => {
+  afterEach(async () => {
+    await clean.students();
+  });
+
+  describe('insert', () => {
+    it('should insert student with new email', async () => {
+      const email = faker.internet.exampleEmail();
+      await dbStudents.insert(email);
+
+      const savedStudent = await db(studentsTable).where('email', email).first();
+      expect(savedStudent).exist;
+      expect(savedStudent.letter).to.be.null;
+      expect(savedStudent.appointment).to.be.null;
+      expect(savedStudent.referral).to.be.null;
+      expect(savedStudent.createdAt).to.exist;
+      expect(savedStudent.updatedAt).to.be.null;
+    });
+
+    it('should ignore with already existing email', async () => {
+      const email = faker.internet.exampleEmail();
+      await dbStudents.insert(email);
+      await dbStudents.insert(email);
+
+      const savedStudent: Student[] = await db(studentsTable).where('email', email);
+      expect(savedStudent).has.length(1);
+      expect(savedStudent[0].letter).to.be.null;
+      expect(savedStudent[0].appointment).to.be.null;
+      expect(savedStudent[0].referral).to.be.null;
+      expect(savedStudent[0].createdAt).to.be.not.null;
+      expect(savedStudent[0].updatedAt).to.be.null;
+    });
+  });
+
+  describe('update', () => {
+    it('should update letter', async () => {
+      const email = faker.internet.exampleEmail();
+      await dbStudents.insert(email);
+      const student = await db(studentsTable).where('email', email).first();
+
+      await dbStudents.updateById(student.id, {
+        letter: true,
+      });
+
+      const savedStudent = await db(studentsTable).where('email', email).first();
+      expect(savedStudent.letter).to.be.true;
+      expect(savedStudent.appointment).to.be.null;
+      expect(savedStudent.referral).to.be.null;
+      expect(savedStudent.updatedAt).to.be.not.null;
+    });
+
+    it('should update appointment', async () => {
+      const email = faker.internet.exampleEmail();
+      await dbStudents.insert(email);
+      const student = await db(studentsTable).where('email', email).first();
+
+      await dbStudents.updateById(student.id, {
+        appointment: false,
+      });
+
+      const savedStudent = await db(studentsTable).where('email', email).first();
+      expect(savedStudent.letter).to.be.null;
+      expect(savedStudent.appointment).to.be.false;
+      expect(savedStudent.referral).to.be.null;
+      expect(savedStudent.updatedAt).to.be.not.null;
+    });
+
+    it('should update referral', async () => {
+      const email = faker.internet.exampleEmail();
+      await dbStudents.insert(email);
+      const student = await db(studentsTable).where('email', email).first();
+
+      await dbStudents.updateById(student.id, {
+        referral: 3,
+      });
+
+      const savedStudent = await db(studentsTable).where('email', email).first();
+      expect(savedStudent.letter).to.be.null;
+      expect(savedStudent.appointment).to.be.null;
+      expect(savedStudent.referral).to.equal(3);
+      expect(savedStudent.updatedAt).to.be.not.null;
+    });
+
+    it('should ignore if id is unknown', async () => {
+      const unknownId = uuidv4();
+      await dbStudents.updateById(unknownId, {
+        referral: 3,
+      });
+    });
+  });
+});
