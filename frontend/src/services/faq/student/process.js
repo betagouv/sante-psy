1ffa89f9d9e8e10561ceebdb0922419787fcefd2
Desoindx <xavier.desoindre@hotmail.fr>
2023-01-30 10:14:41 +0000

    Add question

--- frontend/src/services/faq/student/process.js
+++ frontend/src/services/faq/student/process.js
@@ -2,12 +2,18 @@
 /* eslint-disable max-len */
 module.exports = () => [
   {
+    question: "Comment puis-je me procurer la lettre d'orientation\u00A0?",
+    answer:
+      "Vous pouvez consulter tout médecin généraliste exerçant sur le territoire français, au sein de votre SSU ou extérieur. Le médecin généraliste n'est pas nécessairement votre médecin traitant.",
+    frequent: true,
+  },
+  {
     question: "Que dois contenir la lettre d'orientation\u00A0? Que dois-je dire au médecin généraliste\u00A0?",
     answer:
       'La lettre d\'orientation peut stipuler votre "besoin de suivi psychologique". Il n\'y a pas de mention obligatoire ou de date limite. Vous pouvez parler de vos difficultés ou de votre besoin de consulter un psychologue dans le cadre du dispositif Santé Psy Étudiant.',
   },
   {
-    question: "Je n'ai pas de médecin généraliste, que faire ?",
+    question: "Je n'ai pas de médecin généraliste, que faire\u00A0?",
     answer:
       "La consultation chez un médecin généraliste est obligatoire. Le médecin généraliste n'est pas nécessairement votre médecin traitant. Vous pouvez consulter le médecin généraliste de votre choix : au sein de votre Service de Santé Universitaire (SSU) de proximité, habilité à recevoir des étudiants ou tout autre médecin généraliste extérieur, sur doctolib ou au sein d'un centre médical.",
     frequent: true,
