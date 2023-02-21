bb04cd673e074ba1e2a192da7fc9662d4a63ec57
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2023-02-21 12:57:55 +0100

    fix(student): style for answer & unregister pages

--- frontend/src/components/StudentUnregister/StudentUnregister.jsx
+++ frontend/src/components/StudentUnregister/StudentUnregister.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import Page from 'components/Page/Page';
+import Slice from 'components/Slice/Slice';
 import agent from 'services/agent';
 
 const StudentUnregister = () => {
@@ -13,8 +13,10 @@ const StudentUnregister = () => {
   }, []);
 
   return (
-    <Page
-      title=""
+    <Slice
+    color="secondary"
+    centerText
+    centerTitle
       description={(
         <>
           <div>Ta demande a bien été prise en compte ✅</div>
