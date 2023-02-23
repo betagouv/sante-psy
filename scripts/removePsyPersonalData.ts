e58c5b615871a4375653bae65bd4ce53252af6e9
Sandrine Ricardo <68006779+sandrica89@users.noreply.github.com>
2023-02-23 11:48:20 +0100

    feat(script): remove psy personal data (#565)
    * feat(script): remove psy personal data
    
    * fix(script): add missing exit

---
+++ scripts/removePsyPersonalData.ts
@@ -0,0 +1,64 @@
+import db from '../db/db';
+import { psychologistsTable } from '../db/tables';
+import dbPsychologists from '../db/psychologists';
+
+const removePsyPersonalData = async (dossierNumber: string): Promise<void> => {
+  console.log('Removing personal data for psychologist', dossierNumber);
+
+  try {
+    const psychologist = await dbPsychologists.getById(dossierNumber);
+
+    if (!psychologist) {
+      console.log('Psychologist not found!');
+      console.log("Help: you should specify the dossierNumber, it's a uuid");
+      process.exit(1);
+    }
+
+    if (psychologist.active && psychologist.archived) {
+      console.log('Psychologist is still displayed in public list!');
+      console.log('Help: archive the dossier in DS or ask him/her to deactivate his/her account');
+      process.exit(1);
+    }
+
+    const erasedData = '-';
+    await db(psychologistsTable)
+      .where({ dossierNumber })
+      .update({
+        selfModified: true,
+        firstNames: erasedData,
+        lastName: erasedData,
+        adeli: erasedData,
+        email: erasedData,
+        address: erasedData,
+        city: erasedData,
+        postcode: erasedData,
+        longitude: null,
+        latitude: null,
+        otherAddress: erasedData,
+        otherCity: erasedData,
+        otherPostcode: erasedData,
+        otherLongitude: null,
+        otherLatitude: null,
+        phone: erasedData,
+        website: erasedData,
+        description: erasedData,
+        personalEmail: erasedData,
+        useFirstNames: erasedData,
+        useLastName: erasedData,
+      });
+
+    console.log('Done!');
+    console.log('Remember to also clean data from integrated tools (emailing, DS, etc.)');
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(2);
+  }
+};
+
+if (process.argv.length <= 2) {
+  console.log('Invalid: you should specify psychologist id as argument');
+  process.exit(1);
+}
+
+removePsyPersonalData(process.argv[2]);
