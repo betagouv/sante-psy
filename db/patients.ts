9bd4eb7102a365b8cf28723a4d384d92c53702e9
Sandrine Ricardo <sandrine.ricardo@gmail.com>
2023-03-02 17:06:42 +0100

    fix: university year starts in september

--- db/patients.ts
+++ db/patients.ts
@@ -18,9 +18,10 @@ const getById = async (patientId: string, psychologistId: string): Promise<Patie
 };
 
 const startCurrentUnivYear = (): string => {
+  const SEPTEMBER = 8;
   const currentMonth = new Date().getMonth();
   const currentYear = new Date().getFullYear();
-  if (currentMonth < 6) {
+  if (currentMonth < SEPTEMBER) {
     return `${currentYear - 1}-09-01`;
   }
   return `${currentYear}-09-01`;
