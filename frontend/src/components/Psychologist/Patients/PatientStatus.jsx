a70511c2fcf25004ad22cb7b22102d6316860936
alleytuna <anais.altun@gmail.com>
2024-04-04 12:43:14 +0200

    create right student badges

--- frontend/src/components/Psychologist/Patients/PatientStatus.jsx
+++ frontend/src/components/Psychologist/Patients/PatientStatus.jsx
@@ -1,44 +1,20 @@
 import React from 'react';
-import { Tag } from '@dataesr/react-dsfr';
-
-import classNames from 'classnames';
+import renderBadge from 'components/Badges/generateBadges';
+import useScreenSize from 'src/utils/useScreenSize';
 import styles from './patientStatus.cssmodule.scss';
 
-const PatientStatus = ({ missingInfo, hasTooMuchAppointment, currentYear }) => {
-  const isOk = missingInfo.length === 0 && !hasTooMuchAppointment;
+const PatientStatus = ({ missingInfo, hasReachedMaxAppointment, hasTooMuchAppointment, currentYear }) => {
+  const isSmallScreen = useScreenSize();
+  const isCompleted = !missingInfo.missingPrescriptionInfo && !missingInfo.missingStudentInfo && !hasTooMuchAppointment;
 
   return (
-    <>
-      <div className="fr-col-2 fr-col-md-1 fr-unhidden fr-hidden-sm">
-        <Tag
-          className={classNames(isOk ? styles.complete : styles.incomplete, styles.small)}
-          icon={isOk ? 'ri-check-line' : 'ri-alert-line'}
-          iconPosition="left"
-          size="sm"
-          as="span"
-        />
-      </div>
-      <div className="fr-col-4 fr-col-xl-5 fr-hidden fr-unhidden-sm">
-        <Tag
-          data-test-id={isOk ? 'etudiant-row-complete-info' : 'etudiant-row-missing-info'}
-          className={classNames(isOk ? styles.complete : styles.incomplete, styles.big)}
-          icon={isOk ? 'ri-check-line' : 'ri-alert-line'}
-          iconPosition="left"
-          size="sm"
-          as="span"
-        >
-          {isOk
-            ? 'Dossier complet'
-            : (
-              <>
-                {missingInfo.length !== 0 && `Informations manquantes : ${missingInfo.join(', ')}.`}
-                {missingInfo.length !== 0 && hasTooMuchAppointment && <br />}
-                {hasTooMuchAppointment && `Excès de séances sur ${currentYear}.`}
-              </>
-            )}
-        </Tag>
-      </div>
-    </>
+    <div className={styles.badgeWrapper}>
+      {missingInfo.missingStudentInfo && renderBadge({ badge: 'student_infos', isSmallScreen })}
+      {missingInfo.missingPrescriptionInfo && renderBadge({ badge: 'prescription_infos', isSmallScreen })}
+      {hasReachedMaxAppointment && renderBadge({ badge: 'max', isSmallScreen })}
+      {hasTooMuchAppointment && renderBadge({ badge: 'exceeded', appointmentDate: currentYear, isSmallScreen })}
+      {isCompleted && renderBadge({ badge: 'completed', isSmallScreen })}
+    </div>
   );
 };
 
