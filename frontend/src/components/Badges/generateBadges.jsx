c732ef814f8b7fcdb720b63099fbbecfb78194ad
alleytuna <anais.altun@gmail.com>
2024-04-04 12:44:49 +0200

    add small screen render

--- frontend/src/components/Badges/generateBadges.jsx
+++ frontend/src/components/Badges/generateBadges.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 import { getUnivYear } from 'services/date';
 import { Badge } from '@dataesr/react-dsfr';
 import appointmentBadges from 'src/utils/badges';
+import styles from './generateBadges.cssmodule.scss';
 
-const generateBadgeStyles = (badge, appointmentDate) => {
+const generateBadgeStyles = (badge, appointmentDate, isSmallScreen) => {
   let univYear = null;
+
+  // todo : replace appointmentDate by univYear for appointmentBadges
+
   if (
     appointmentDate
     && badge === appointmentBadges.exceeded
   ) {
     univYear = getUnivYear(appointmentDate);
   }
+  if (
+    appointmentDate
+    && badge === 'exceeded'
+  ) {
+    univYear = appointmentDate;
+  }
+
   const badgeStyles = {
     first: {
       text: '1re séance',
@@ -18,7 +29,7 @@ const generateBadgeStyles = (badge, appointmentDate) => {
       icon: 'fr-icon-info-fill fr-icon--sm',
     },
     max: {
-      text: 'Maximum de séances atteint',
+      text: isSmallScreen ? 'Max' : 'Maximum de séances atteint',
       severity: 'warning',
       icon: 'fr-icon-warning-fill fr-icon--sm',
     },
@@ -28,32 +39,41 @@ const generateBadgeStyles = (badge, appointmentDate) => {
       icon: 'fr-icon-info-fill fr-icon--sm',
     },
     exceeded: {
-      text: `Excès de séances ${univYear || ''}`,
+      text: isSmallScreen ? 'Excès' : `Excès de séances ${univYear || ''}`,
       severity: 'warning',
       icon: 'fr-icon-warning-fill fr-icon--sm',
     },
     student_infos: {
-      text: 'Dossier étudiant à compléter',
+      text: isSmallScreen ? 'Dossier' : 'Dossier étudiant à compléter',
       severity: 'info',
       icon: 'fr-icon-info-fill fr-icon--sm',
     },
     prescription_infos: {
-      text: "Lettre d'orientation à compléter",
+      text: isSmallScreen ? 'Lettre' : "Lettre d'orientation à compléter",
       severity: 'new',
       icon: 'fr-icon-info-fill fr-icon--sm',
+    },
+    completed: {
+      text: 'à jour',
+      severity: 'success',
+      icon: 'fr-icon-success-fill fr-icon--sm',
     },
   };
   return badgeStyles[badge];
 };
 
-const renderBadge = ({ badge, appointmentDate }) => {
+const renderBadge = ({ badge, appointmentDate, isSmallScreen }) => {
   if (!badge || badge === appointmentBadges.other) {
     return null;
   }
 
-  const { icon, text, severity } = generateBadgeStyles(badge, appointmentDate);
+  const { icon, text, severity } = generateBadgeStyles(
+    badge,
+    appointmentDate,
+    isSmallScreen,
+  );
 
-  return <Badge icon={icon} text={text} type={severity} />;
+  return <Badge icon={icon} text={text} type={severity} className={styles.badgeWrapper} />;
 };
 
 export default renderBadge;
