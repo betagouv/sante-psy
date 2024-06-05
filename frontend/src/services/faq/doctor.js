b4f0fd01d24829861f74cfd1c09f413ecad6db13
donia benharara <donia.benharara@pathtech.fr>
2024-06-05 11:49:03 +0200

    fix wording

--- frontend/src/services/faq/doctor.js
+++ frontend/src/services/faq/doctor.js
@@ -3,7 +3,7 @@
 module.exports = () => [
   {
     question: 'En tant que médecin généraliste je suis contacté par un étudiant. Que dois-je faire\u00A0?',
-    answer: 'En cas de besoin identifié, vous pouvez orienter l’étudiant, muni d’une lettre d\'orientation de votre part, vers l’un des psychologues partenaires du dispositif, \
+    answer: 'En cas de besoin identifié, vous pouvez orienter l’étudiant, muni d’une lettre d\'orientation de votre part, vers l’un des psychologues partenaires du dispositif. \
     Cette lettre d\'orientation permet à l’étudiant de bénéficier jusqu’à 8 séances, entièrement prises en charge par l’université.\
     L’étudiant doit vous présenter sa carte d’étudiant ou tout document équivalent attestant de son statut étudiant.',
   },
