9982b55845d467bd80cccc6fa2aacd7fc39b2020
donia benharara <donia.benharara@pathtech.fr>
2024-06-17 19:20:26 +0200

    lint fix

--- frontend/src/components/Psychologist/Appointments/Appointments.jsx
+++ frontend/src/components/Psychologist/Appointments/Appointments.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { HashLink } from 'react-router-hash-link';
-import { Button, Table, Callout, CalloutText, Icon, TextInput } from '@dataesr/react-dsfr';
+import { Button, Table, Icon, TextInput } from '@dataesr/react-dsfr';
 
 import MonthPicker from 'components/Date/MonthPicker';
 
