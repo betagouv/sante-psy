9600d3bbfcc9345031281d5bec4d2d93a3118027
donia benharara <donia.benharara@pathtech.fr>
2025-02-24 13:46:47 +0100

    increase wait test tuto

--- frontend/cypress/e2e/tutorial.cy.js
+++ frontend/cypress/e2e/tutorial.cy.js
@@ -58,7 +58,7 @@ describe('Global tutorial', () => {
   it('should pass tuto', () => {
     cy.get('[data-test-id="close-tutorial"]')
       .click();
-    cy.wait(500);
+    cy.wait(1000);
     cy.get('[data-test-id="next-step"]')
       .should('not.exist');
 
