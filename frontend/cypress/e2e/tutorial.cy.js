5d1584852c04a6703c1c54a8b7a18c1921d3b4ff
donia benharara <donia.benharara@pathtech.fr>
2025-02-24 13:28:41 +0100

    remove only

--- frontend/cypress/e2e/tutorial.cy.js
+++ frontend/cypress/e2e/tutorial.cy.js
@@ -55,7 +55,7 @@ describe('Global tutorial', () => {
       .should('not.exist');
   });
 
-  it.only('should pass tuto', () => {
+  it('should pass tuto', () => {
     cy.get('[data-test-id="close-tutorial"]')
       .click();
     cy.wait(500);
