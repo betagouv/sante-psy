44cb47241c977c34ee65b03a655e51d848fae725
alleytuna <anais.altun@gmail.com>
2025-02-26 18:48:20 +0100

    chore(lint)

--- frontend/src/components/Landing/Statistics.jsx
+++ frontend/src/components/Landing/Statistics.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Slice from 'components/Slice/Slice';
+import agent from 'services/agent';
 import Statistic from './Statistic';
 import styles from './statistics.cssmodule.scss';
-import agent from 'services/agent';
 
 const Statistics = () => {
   const [statistics, setStatistics] = useState([]);
@@ -12,29 +12,30 @@ const Statistics = () => {
   }, []);
 
   return (
-  <Slice>
-    <div className={styles.header}>
-      <div className={styles.title}>
-        <b>Santé Psy Étudiant</b>
-        {' '}
-        à votre écoute
+    <Slice>
+      <div className={styles.header}>
+        <div className={styles.title}>
+          <b>Santé Psy Étudiant</b>
+          {' '}
+          à votre écoute
+        </div>
+        <div className={styles.description}>
+          Pour tout étudiant de l’enseignement supérieur,
+          dont la formation est reconnue par le ministère de l’Enseignement supérieur et de la Recherche.
+        </div>
       </div>
-      <div className={styles.description}>
-        Pour tout étudiant de l’enseignement supérieur,
-        dont la formation est reconnue par le ministère de l’Enseignement supérieur et de la Recherche.
-      </div>
-    </div>
-    <div className={styles.separator} />
-    <div className={styles.content}>
-      {statistics.map(statistic => (
-        <Statistic
-          key={statistic.label}
-          value={statistic.value}
-          description={statistic.label}
+      <div className={styles.separator} />
+      <div className={styles.content}>
+        {statistics.map(statistic => (
+          <Statistic
+            key={statistic.label}
+            value={statistic.value}
+            description={statistic.label}
           />
-      ))}
-    </div>
-  </Slice>
-)}
+        ))}
+      </div>
+    </Slice>
+  );
+};
 
 export default Statistics;
