1a511a227e676735c357d025bd01d5ba8a5557d9
alleytuna <anais.altun@gmail.com>
2025-03-04 19:31:28 +0100

    feat(contactPage): finale page architecture

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -3,75 +3,81 @@ import { observer } from 'mobx-react';
 import Page from 'components/Page/Page';
 import ServicesList from 'components/OtherServices/ServicesList';
 import { Button } from '@dataesr/react-dsfr';
+import { useNavigate } from 'react-router-dom';
 import ContactCards from './Cards';
 import styles from './contact.cssmodule.scss';
 
-const Contact = () => (
-  <>
-    <Page
-      textContent
-      breadCrumbs={[{ href: '/', label: 'Accueil' }]}
-      currentBreadCrumb="Nous contacter"
-      title={(
-        <>
-          Vous avez une
-          {' '}
-          <b>question</b>
-          {' '}
-          ?
-        </>
+const Contact = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Page
+        textContent
+        breadCrumbs={[{ href: '/', label: 'Accueil' }]}
+        currentBreadCrumb="Nous contacter"
+        title={(
+          <>
+            Vous avez une
+            {' '}
+            <b>question</b>
+            {' '}
+            ?
+          </>
     )}
-      description={(
-        <>
-          Consultez nos questions fréquentes,
-          la réponse à votre question s&apos;y trouve peut-être.
-        </>
-    )}
-  >
-      <ContactCards />
-    </Page>
-    <Page
-      textContent
-      title={(
-        <>
-          Besoin d&apos;une
-          {' '}
-          <b>écoute</b>
-          {' '}
-          immédiate
-        </>
-    )}
-      description={(
-        <>
-          Plusieurs lignes d&apos;écoute sont là pour t&apos;écouter et te conseiller
-        </>
+        description={(
+          <>
+            Consultez nos questions fréquentes,
+            la réponse à votre question s&apos;y trouve peut-être.
+          </>
     )}
   >
-      <ServicesList urgentServices />
-    </Page>
-    <div className={styles.purpleBanner}>
-      <h1 className={styles.smallTitle}>
-        Vous ne trouvez pas de
-        {' '}
-        <b>réponse</b>
-        {' '}
-        satisfaisante ?
-      </h1>
-      <p>
-        Contactez-nous directement par e-mail pour que nous puissions trouver une solution.
-      </p>
-      <div>
-        <Button
-          icon="ri-mail-line"
-          iconPosition="right"
-          className={styles.button}
-          onClick={() => {}}
+        <ContactCards />
+      </Page>
+      <Page
+        textContent
+        title={(
+          <>
+            Besoin d&apos;une
+            {' '}
+            <b>écoute</b>
+            {' '}
+            immédiate
+          </>
+    )}
+        description={(
+          <>
+            Plusieurs lignes d&apos;écoute sont là pour t&apos;écouter et te conseiller
+          </>
+    )}
+  >
+        <ServicesList urgentServices />
+      </Page>
+      <div className={styles.purpleBanner}>
+        <h1 className={styles.smallTitle}>
+          Vous ne trouvez pas de
+          {' '}
+          <b>réponse</b>
+          {' '}
+          satisfaisante ?
+        </h1>
+        <p>
+          Contactez-nous directement par e-mail pour que nous puissions trouver une solution.
+        </p>
+        <div className={styles.hover}>
+          <Button
+            secondary
+            icon="ri-mail-line"
+            iconPosition="right"
+            className={styles.button}
+            onClick={() => navigate('/contact/form')}
         >
-          Nous contacter par mail
-        </Button>
+            Nous contacter par mail
+          </Button>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default observer(Contact);
