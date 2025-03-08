e464fde76e994feac634385dc04500104dfd3fff
alleytuna <anais.altun@gmail.com>
2025-03-08 19:16:53 +0100

    review1

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import { observer } from 'mobx-react';
 import Page from 'components/Page/Page';
 import ServicesList from 'components/OtherServices/ServicesList';
-import { Button } from '@dataesr/react-dsfr';
+import { Button, Row } from '@dataesr/react-dsfr';
 import { useNavigate } from 'react-router-dom';
-import ContactCards from './Cards';
+import Card from 'components/Card/Card';
 import styles from './contact.cssmodule.scss';
+
+const cards = [
+  { imgSrc: 'student', title: 'Je suis étudiant', section: 'etudiant' },
+  { imgSrc: 'psychologist', title: 'Je suis psychologue', section: 'psychologue' },
+  { imgSrc: 'university', title: 'Je suis une école', section: 'ecole' },
+];
+
+const contactCardStyle = {
+  card: 'contactCard',
+  image: 'contactCardImg',
+  title: 'contactCardTitle',
+};
 
 const Contact = () => {
   const navigate = useNavigate();
@@ -18,39 +30,37 @@ const Contact = () => {
         currentBreadCrumb="Nous contacter"
         title={(
           <>
-            Vous avez une
-            {' '}
-            <b>question</b>
+            <b>Vous avez une question</b>
             {' '}
             ?
           </>
-    )}
-        description={(
-          <>
-            Consultez nos questions fréquentes,
-            la réponse à votre question s&apos;y trouve peut-être.
-          </>
-    )}
-  >
-        <ContactCards />
+)}
+        description={<>Consultez nos questions fréquentes, la réponse à votre question s&apos;y trouve peut-être.</>}
+      >
+        <Row className={styles.cardContainer}>
+          {cards.map(card => (
+            <Card
+              customStyles={contactCardStyle}
+              key={card.section}
+              title={card.title}
+              image={`contact/${card.imgSrc}.png`}
+              link={`/faq?section=${card.section}`}
+              fullClickable
+    />
+          ))}
+        </Row>
       </Page>
       <Page
         textContent
         title={(
           <>
-            Besoin d&apos;une
-            {' '}
-            <b>écoute</b>
+            <b>Besoin d&apos;une écoute</b>
             {' '}
             immédiate
           </>
-    )}
-        description={(
-          <>
-            Plusieurs lignes d&apos;écoute sont là pour t&apos;écouter et te conseiller
-          </>
-    )}
-  >
+)}
+        description={<>Plusieurs lignes d&apos;écoute sont là pour t&apos;écouter et te conseiller</>}
+      >
         <ServicesList urgentServices />
       </Page>
       <div className={styles.purpleBanner}>
@@ -71,7 +81,7 @@ const Contact = () => {
             iconPosition="right"
             className={styles.button}
             onClick={() => navigate('/contact/formulaire')}
-        >
+          >
             Nous contacter par mail
           </Button>
         </div>
