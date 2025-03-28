704e6557d5257055bff6491852149de849179cef
Anaïs A. <59049715+alleytuna@users.noreply.github.com>
2025-03-28 18:40:21 +0100

    fix(genderMigration): gender can be nullable (#720)

--- migrations/20250324112727_add_patients_gender.ts
+++ migrations/20250324112727_add_patients_gender.ts
@@ -2,7 +2,7 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.table('patients', (table) => {
-    table.text('gender').notNullable();
+    table.text('gender');
   });
 }
 
