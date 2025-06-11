7d3ae6b4bd6bc7c3551181cdfc2842aa3521ffa6
alleytuna <anais.altun@gmail.com>
2025-06-11 11:53:57 +0200

    insequable white space

---
+++ frontend/src/components/Psychologist/PsyDashboard/UnivSection/Contact.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Button } from '@dataesr/react-dsfr';
+import styles from './univSection.cssmodule.scss';
+
+const UnivContact = ({ university }) => {
+  const checkIsEmpty = value => (value?.trim() ? value : '-');
+
+  const fullAddress = university
+    ? `${checkIsEmpty(university.address)}, ${checkIsEmpty(university.postal_code)} ${checkIsEmpty(university.city)}`
+    : '-';
+
+  const renderEmailIcon = (label, emailListRaw) => {
+    const emails = emailListRaw?.split(',').map(email => email.trim()).filter(Boolean) || [];
+    const emailDisplay = emails.length ? emails.join(', ') : '-';
+    const mailto = emails.length ? `mailto:${emails.join(',')}` : null;
+
+    return (
+      <section className={styles.emailRow}>
+        <div className={styles.emailText}>
+          <b>
+            {label}&nbsp;:
+          </b>
+          &nbsp;
+          <span>{emailDisplay}</span>
+        </div>
+        {mailto && (
+          <Button
+            tertiary
+            size="lg"
+            icon="ri-mail-line"
+            className={styles.mailButton}
+            title={`Envoyer un mail à ${emails}`}
+            onClick={() => { window.location.href = mailto; }}
+          />
+        )}
+      </section>
+    );
+  };
+
+  return (
+    <section className={styles.univBillingCard}>
+      {university ? (
+        <div>
+          <h2>Contact universitaire</h2>
+          <b>{`Université ${checkIsEmpty(university.name)}`}</b>
+          <p>{fullAddress}</p>
+          {renderEmailIcon('Facturation', university.billingEmail)}
+          {renderEmailIcon('Convention', university.emailSSU)}
+        </div>
+      ) : (
+        <div>
+          <p>Vous n&apos;êtes rattaché à aucune université</p>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default UnivContact;
