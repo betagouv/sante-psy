e8da278fa96427c8735222567f868b0912ab8be8
donia benharara <donia.benharara@pathtech.fr>
2025-09-04 17:06:56 +0200

    simplify psy facturation

---
+++ frontend/src/components/Tutorial/Steps/billingInfo.js
@@ -0,0 +1,10 @@
+const steps = [
+  {
+    placement: 'center',
+    target: 'body',
+    title: 'Informations de facturation',
+    content: 'Cette page vous permet de gérér vos informations de facturation.',
+  }
+];
+
+export default steps;
