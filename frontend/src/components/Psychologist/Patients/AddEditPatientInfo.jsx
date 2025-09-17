186234ac362854d0135b2327017885faf2f378b2
Anaïs A. <59049715+alleytuna@users.noreply.github.com>
2025-09-17 11:12:55 +0200

    add ine provisoire format in validator (#748)

--- frontend/src/components/Psychologist/Patients/AddEditPatientInfo.jsx
+++ frontend/src/components/Psychologist/Patients/AddEditPatientInfo.jsx
@@ -16,6 +16,7 @@ const PatientInfo = ({ patient, changePatient, handleFormErrors }) => {
       /^\d{10}[A-HJ-NPR-Z]$/, // INE-BEA
       /^[0-9A-Z]{10}\d$/, // INE-Base 36
       /^\d{4}[A]\d{5}[A-HJ-NPR-Z]$/, // INE-SIFA
+      /^\d{4}[D]\d{5}[A-HJ-NPR-Z]$/i, // INE provisoire
     ];
 
     const isValid = patterns.some(pattern => pattern.test(value));
