fd768e8c477850d6c9ebb97c344a2bda11fadbb5
alleytuna <anais.altun@gmail.com>
2026-02-12 18:56:48 +0100

    chore: fix missing import

--- routers/protected.ts
+++ routers/protected.ts
@@ -14,6 +14,7 @@ import loginController from '../controllers/loginController';
 
 import access from '../utils/access';
 import config from '../utils/config';
+import studentsController from '../controllers/studentsController';
 
 const router = express.Router();
 
