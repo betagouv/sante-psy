92907e5da8ccf1e172d7767f3bb1653eefca6aac
tristan-gueguen <tristan.gueguen@gmail.com>
2026-05-28 16:00:20 +0200

    chore(lint): disable (what i think are) stupid rules

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -63,6 +63,9 @@ module.exports = {
     'react/static-property-placement': 'off',
     'react/function-component-definition': 'off',
     "import/order": "off",
+    'import/prefer-default-export': 'off',
+    'react/no-unescaped-entities': 'off',
+    'no-nested-ternary': 'off'
   },
   settings: {
     'import/resolver': { webpack: { config: 'config/webpack.base.js' } },
