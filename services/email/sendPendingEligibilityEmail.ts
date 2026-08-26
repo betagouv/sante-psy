bfffa672675dacf7b50d789868ba2e5938317a07
tristan-gueguen <tristan.gueguen@gmail.com>
2026-08-26 22:39:44 +0200

    feat(signin,students): renamed email to pending eligibility

--- services/email/sendPendingEligibilityEmail.ts
+++ services/email/sendPendingEligibilityEmail.ts
@@ -3,9 +3,13 @@ import send from '../../utils/email';
 import CustomError from '../../utils/CustomError';
 import config from '../../utils/config';
 
-export const sendNotEligibleEmail = async (email: string): Promise<void> => {
+export const sendPendingEligibilityEmail = async (
+  email: string,
+): Promise<void> => {
   try {
-    const html = await ejs.renderFile(`./views/emails/studentNotEligible.ejs`);
+    const html = await ejs.renderFile(
+      `./views/emails/studentPendingEligibility.ejs`,
+    );
     await send(
       email,
       `${config.appName} - Ton éligibilité est en cours d’instruction`,
@@ -16,7 +20,7 @@ export const sendNotEligibleEmail = async (email: string): Promise<void> => {
     throw err instanceof CustomError
       ? err
       : new CustomError(
-          "Erreur lors de l'envoi du mail de non éligibilité",
+          "Erreur lors de l'envoi du mail d'éligibilité en cours d'instruction",
           500,
         );
   }
