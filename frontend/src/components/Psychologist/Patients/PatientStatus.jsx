608db89a5c3a0f49c2a63d8588f3e6f086c3fd0f
tristan-gueguen <tristan.gueguen@gmail.com>
2026-09-04 13:00:47 +0200

    feat(psy,patient,eligibility): refacto alerts

--- frontend/src/components/Psychologist/Patients/PatientStatus.jsx
+++ frontend/src/components/Psychologist/Patients/PatientStatus.jsx
@@ -2,74 +2,86 @@ import React from 'react';
 import { Alert } from '@dataesr/react-dsfr';
 import getBadgeInfos from 'src/utils/badges';
 
+export const StatusNoStudentAccount = () => (
+  <Alert
+    type="warning"
+    title="Pas de compte étudiant"
+    description="L'étudiant doit créer un compte pour bénéficier du dispositif SPE."
+  />
+);
+
+export const StatusNotEligible = () => (
+  <Alert
+    type="error"
+    title="Non éligible"
+    description="Cet étudiant ne semble pas éligible. Une erreur ? Contactez le support."
+  />
+);
+
+export const StatusPendingEligibility = () => (
+  <Alert
+    type="info"
+    title="En cours d'instruction"
+    description="L’éligibilité de cet étudiant est en cours d'instruction. Le statut se mettra à jour automatiquement."
+  />
+);
+
+export const StatusIncompleteProfile = () => (
+  <Alert
+    type="warning"
+    title="Profil pas à jour"
+    description="L'étudiant doit mettre à jour son compte et télécharger son justificatif de scolarité pour la nouvelle année."
+  />
+);
+
+export const StatusCheckCertif = () => (
+  <Alert
+    type="info"
+    title="Justificatif à valider"
+    description="Le justificatif de scolarité doit être vérifié et sa validité (année / 
+identité) confirmée au moment de la déclaration de la 1ère séance."
+  />
+);
+
+export const StatusUpToDate = () => (
+  <Alert
+    type="success"
+    title="À jour"
+    description="Vous pouvez déclarer des séances pour cet étudiant."
+  />
+);
+
 const PatientStatus = ({ patient }) => {
   const badges = getBadgeInfos();
 
   // Pas de compte étudiant
   if (patient.badges.includes(badges.no_student_account.key)) {
-    return (
-      <Alert
-        type="warning"
-        title="Pas de compte étudiant"
-        description="L'étudiant doit créer un compte pour bénéficier du dispositif SPE."
-      />
-    );
+    return <StatusNoStudentAccount />;
   }
 
   // Non éligible
   if (patient.badges.includes(badges.not_eligible.key)) {
-    return (
-      <Alert
-        type="error"
-        title="Non éligible"
-        description="Cet étudiant ne semble pas éligible. Une erreur ? Contactez le support."
-      />
-    );
+    return <StatusNotEligible />;
   }
 
   // En cours d'instruction
   if (patient.badges.includes(badges.pending_eligibility.key)) {
-    return (
-      <Alert
-        type="info"
-        title="En cours d'instruction"
-        description="L’éligibilité de cet étudiant est en cours d'instruction. Le statut se mettra à jour automatiquement."
-      />
-    );
+    return <StatusPendingEligibility />;
   }
 
   // Profil incomplet
   if (patient.badges.includes(badges.incomplete_profile.key)) {
-    return (
-      <Alert
-        type="warning"
-        title="Profil pas à jour"
-        description="L'étudiant doit mettre à jour son compte et télécharger son justificatif de scolarité pour la nouvelle année."
-      />
-    );
+    return <StatusIncompleteProfile />;
   }
 
   // Justificatif à valider
   if (patient.badges.includes(badges.check_certif.key)) {
-    return (
-      <Alert
-        type="info"
-        title="Justificatif à valider"
-        description="Le justificatif de scolarité doit être vérifié et sa validité (année / 
-identité) confirmée au moment de la déclaration de la 1ère séance."
-      />
-    );
+    return <StatusCheckCertif />;
   }
 
   // Profil a jour
   if (patient.badges.includes(badges.completed.key)) {
-    return (
-      <Alert
-        type="success"
-        title="À jour"
-        description="Vous pouvez déclarer des séances pour cet étudiant."
-      />
-    );
+    return <StatusUpToDate />;
   }
 
   return null;
